16c417607202f6935503f3b60b65927d3c65efd6
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-18 02:59:50 +0100

    contrib/server: link sockets and db

--- packages/contrib/server.js
+++ packages/contrib/server.js
@@ -1,18 +1,25 @@
 const { createServer } = require("http");
 const next = require("next");
 
+const db = require("./db");
 const routes = require("./routes");
+const sockets = require("./sockets");
 
 const { NODE_ENV, PORT } = process.env;
 const environment = NODE_ENV !== undefined ? NODE_ENV : "development";
+const dbUri = require("../../knexfile")[environment].connection;
 const port = PORT !== undefined ? parseInt(PORT) : 3100;
 
 const app = next({ dev: environment !== "production" });
 
 async function start() {
   await app.prepare();
+  const pool = await db(dbUri);
 
   const server = createServer((req, res) => routes(app, req, res));
+  // Link websocket listeners
+  sockets(server, pool);
+
   server.listen(port, err => {
     if (err) throw err;
 
