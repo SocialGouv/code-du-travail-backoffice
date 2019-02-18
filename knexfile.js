9b906e6010d58bac84316ee0a057a34152e1f8b9
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-18 13:18:43 +0100

    knexfile: inject .env config

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,19 @@
+require('dotenv').config()
+
 module.exports = {
   development: {
     client: 'postgresql',
     connection: 'postgres://postgres@localhost:5432/cdtn',
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      tableName: 'knex_migrations',
+    },
+  },
 }
