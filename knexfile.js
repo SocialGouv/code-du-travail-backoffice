72d00db89694abb02f883bf193a8bd1e9f48a434
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-18 11:07:42 +0100

    knexfile: fix database name typo

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'postgresql',
-    connection: 'postgres://postgres@localhost:5432/cndt',
+    connection: 'postgres://postgres@localhost:5432/cdtn',
     migrations: {
       tableName: 'knex_migrations'
     }
