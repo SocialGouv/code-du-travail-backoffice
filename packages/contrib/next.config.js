988db90ca35917a96f4b0b5e56becc5fa4bfc566
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-21 17:33:53 +0100

    contrib/next: add url loader in config

--- packages/contrib/next.config.js
+++ packages/contrib/next.config.js
@@ -1,3 +1,30 @@
+const merge = require("webpack-merge");
 const withCss = require("@zeit/next-css");
 
-module.exports = withCss();
+module.exports = withCss({
+  webpack: config => ({
+    ...config,
+    module: merge(config.module, {
+      rules: [
+        {
+          test: /\.(gif|jpe?g|png|svg)$/,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                limit: 8192,
+                fallback: {
+                  loader: "file-loader",
+                  options: {
+                    publicPath: "/_next/static/images",
+                    outputPath: "static/images"
+                  }
+                }
+              }
+            }
+          ]
+        }
+      ]
+    })
+  })
+});
