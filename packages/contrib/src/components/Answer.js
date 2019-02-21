00b5c81917b04f7c93a1b790339a63534a71d44c
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-21 13:10:23 +0100

    contrib/src/components/Answer: create

---
+++ packages/contrib/src/components/Answer.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactTooltip from "react-tooltip";
+import { Flex } from "rebass";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: white;
+  border-radius: 0.4rem;
+  cursor: pointer;
+  margin: 1rem;
+  padding: 0.5rem 0.5rem 0.5rem 0.75rem;
+`;
+
+const Label = styled.div`
+  color: #c2c2c2;
+  font-size: 0.8rem;
+  font-weight: 600;
+`;
+const Reference = styled.div`
+  background-color: #606060;
+  color: white;
+  font-size: 0.8rem;
+  font-weight: 600;
+  opacity: 0.75;
+  padding: 0.25rem 0.5rem;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+const Tooltip = styled(ReactTooltip)`
+  padding: 0.25rem 0.5rem;
+  width: 360px;
+`;
+
+export default ({ data, onClick }) => (
+  <Container key={data.id} onClick={onClick}>
+    <Flex justifyContent="space-between">
+      <Label>Réponse à rédiger</Label>
+      <Reference data-tip={data.labor_agreement.name}>
+        {data.labor_agreement.idcc}
+      </Reference>
+    </Flex>
+    {data.question.value}
+    <Tooltip />
+  </Container>
+);
