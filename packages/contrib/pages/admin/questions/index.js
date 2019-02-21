da6480521c914cb7b307de84c0605c962c1fdb6c
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-21 13:16:04 +0100

    temp

---
+++ packages/contrib/pages/admin/questions/index.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import styled from "styled-components";
+import React from "react";
+import Router from "next/router";
+
+import Main from "../../../src/layouts/main";
+
+const Row = styled.div`
+  border: dashed 1px grey;
+  cursor: pointer;
+  font-weight: 600;
+  margin: 0.5rem;
+  padding: 0.5rem;
+`;
+
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true
+    };
+
+    this.questions = [];
+  }
+
+  componentDidMount() {
+    if (this.state.isLoading) {
+      const select = "select=*";
+      axios
+        .get(`http://localhost:3200/questions?${select}&limit=10`)
+        .then(({ data }) => {
+          this.questions = data;
+          this.setState({ isLoading: false });
+        })
+        .catch(console.warn);
+    }
+  }
+
+  editQuestion(questionId) {
+    Router.push(`/admin/questions/${questionId}`);
+  }
+
+  getContent() {
+    switch (true) {
+      case this.state.isLoading:
+        return <p>Fetching questions...</p>;
+
+      default:
+        return this.getQuestions();
+    }
+  }
+
+  getQuestions() {
+    return this.questions.map(question => (
+      <Row key={question.id} onClick={() => this.editQuestion(question.id)}>
+        {question.value}
+      </Row>
+    ));
+  }
+
+  render() {
+    return <Main>{this.getContent()}</Main>;
+  }
+}
---
+++ packages/contrib/pages/admin/questions/new.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+import React from "react";
+import { Button as ReButton } from "rebass";
+import styled from "styled-components";
+
+import Main from "../../../src/layouts/main";
+
+const Form = styled.form`
+  /* box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1; */
+`;
+
+const Textarea = styled.textarea`
+  background-color: #315659;
+  border: 0;
+  color: #ffffff;
+  flex-grow: 1;
+  font-family: inherit;
+  font-weight: 600;
+  font-size: 1.25rem;
+  line-height: 1.5;
+  padding: 0.45rem 0.9rem;
+`;
+
+const Button = styled(ReButton)`
+  background-color: #2978a0;
+  border-radius: 0;
+  cursor: pointer;
+`;
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: ""
+    };
+
+    this.updateValue = this.updateValue.bind(this);
+  }
+
+  updateValue(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  save() {
+    const uri = `http://localhost:3200/answers?id=eq.${this.props.id}`;
+    const data = {
+      value: this.value
+    };
+    const config = {
+      headers: { Authorization: `Bearer ${sessionStorage.getItem("jwt")}` }
+    };
+
+    axios.patch(uri, data, config).catch(console.warn);
+  }
+
+  render() {
+    return (
+      <Main>
+        <Form flexDirection="row" onSubmit={() => void 0}>
+          <Textarea onChange={this.updateValue} />
+          <Button>Créer une nouvelle question</Button>
+        </Form>
+      </Main>
+    );
+  }
+}
