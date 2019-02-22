7218c30ff39ea29196e853a6fe85d0444222e69d
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-22 12:47:02 +0100

    seeds/00-empty-tables: empty tags table

--- seeds/00-empty-tables.js
+++ seeds/00-empty-tables.js
@@ -1,6 +1,9 @@
 exports.seed = async knex => {
-  await knex("api.answers").del();
-  await knex("api.questions").del();
-  await knex("api.labor_agreements").del()
-  await knex("basic_auth.users").del();
+  await knex('answers_tags').del()
+  await knex('questions_tags').del()
+  await knex('api.answers').del()
+  await knex('api.questions').del()
+  await knex('api.tags').del()
+  await knex('api.labor_agreements').del()
+  await knex('basic_auth.users').del()
 }
