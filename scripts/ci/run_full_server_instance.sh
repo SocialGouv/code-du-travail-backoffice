c124ce623357914199c3df7441e192fcc4538462
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-25 16:12:02 +0100

    scripts/ci/run_full_server_instance: build & run in prod mode instead of dev

--- scripts/ci/run_full_server_instance.sh
+++ scripts/ci/run_full_server_instance.sh
@@ -4,6 +4,7 @@ while ! sudo lsof -Pi :5432 -sTCP:LISTEN -t; do sleep 1; done
 # Wait for PostgREST port to be used:
 while ! sudo lsof -Pi :3200 -sTCP:LISTEN -t; do sleep 1; done
 
-yarn dev &
+yarn build
+yarn start &
 # Wait for server port port to be used:
 while ! sudo lsof -Pi :3100 -sTCP:LISTEN -t; do sleep 1; done
