dd64c39f2e96b0dbdaeccae175ef4cec2c85c6cc
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-27 17:48:39 +0100

    contrib/src/components/Editor: add test

---
+++ packages/contrib/src/components/__tests__/Editor.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { render } from "react-testing-library";
+import Editor from "../Editor";
+
+// TODO Is it possible to simulate a content edition (input/change event)?
+describe("<Editor />", () => {
+  const onChange = jest.fn();
+  const props = {
+    defaultValue: `
+## Guerre et Paix
+
+### Citation
+
+> Il m'est arrivé de sentir que tout allait bien pour moi, que tout le
+> monde était gai, et aussitôt l'idée me traversait l'esprit qu'il ne se
+> passerait plus rien et que tout était absurde.
+
+*Léon Tolstoï*
+
+<u>Ceci</u> est <u>important</u>.
+
+Ceci est très **important**.
+    `,
+    label: "A label",
+    onChange
+  };
+  const { container } = render(<Editor {...props} />);
+
+  it("should match snapshot", () => {
+    // We need to disable this attribute since it's automatically generated and
+    // we can't have any control over it:
+    // eslint-disable-next-line prettier/prettier
+    container.innerHTML = container.innerHTML
+      .replace(/ medium-editor-index="[^"]{36}"/, "");
+
+    expect(container).toMatchSnapshot();
+  });
+});
