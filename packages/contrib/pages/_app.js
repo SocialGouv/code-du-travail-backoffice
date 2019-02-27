206c6e437f8523ed097fe0fe11b3f053245c8cee
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-27 11:38:56 +0100

    contrib/pages/_app: remove useless auth checking

--- packages/contrib/pages/_app.js
+++ packages/contrib/pages/_app.js
@@ -1,52 +1,20 @@
 import App, { Container } from "next/app";
-import Router from "next/router";
 import React from "react";
 import { Provider } from "react-redux";
 
 import withReduxStore from "../src/lib/with-redux-store";
 
 class MainApp extends App {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isMounted: false
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ isMounted: true });
-
-    if (this.props.router.asPath !== "/" && !this.isAutenticated()) {
-      Router.push("/");
-    }
-  }
-
-  isAutenticated() {
-    const jwt = sessionStorage.getItem("jwt");
-
-    return typeof jwt === "string" && jwt.length > 0;
-  }
-
   render() {
     const { Component, pageProps, reduxStore } = this.props;
 
-    switch (true) {
-      case this.props.router.asPath !== "/" && !this.state.isMounted:
-        return <p>Loading...</p>;
-
-      case this.props.router.asPath !== "/" && !this.isAutenticated():
-        return <p>Redirecting to login page...</p>;
-
-      default:
-        return (
-          <Container>
-            <Provider store={reduxStore}>
-              <Component {...pageProps} />
-            </Provider>
-          </Container>
-        );
-    }
+    return (
+      <Container>
+        <Provider store={reduxStore}>
+          <Component {...pageProps} />
+        </Provider>
+      </Container>
+    );
   }
 }
 
