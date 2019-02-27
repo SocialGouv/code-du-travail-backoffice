9b72336d81677e9626465f980e26be92827fe32f
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-27 16:24:39 +0100

    contrib/src/components/Editor: integrate Markdown lib

--- packages/contrib/src/components/Editor.js
+++ packages/contrib/src/components/Editor.js
@@ -1,19 +1,37 @@
 import { omit } from "ramda";
 import React from "react";
 
+import Markdown from "../lib/Markdown";
+
+const markdown = new Markdown();
+
 /**
- * React wrapper for medium-editor plugin.
+ * React wrapper for medium-editor plugin converting the HTML source into
+ * Markdown.
+ *
+ * @description
+ * The `defaultValue` prop is expecting a string of Markdown.
  */
 export default class Editor extends React.Component {
   componentDidMount() {
     // We have to load medium-editor here because of the global `document`
     // variable check done by the original library.
     const MediumEditor = require("medium-editor");
-    this.medium = new MediumEditor(this.el, {
+
+    this.medium = new MediumEditor(this.$editor, {
+      disableDoubleReturn: true,
+      disableExtraSpaces: true,
+      elementsContainer: this.$container,
+      spellcheck: false,
+      updateOnEmptySelection: true,
       placeholder: {
         text: "Commencer ici votre réponse."
+      },
+      toolbar: {
+        static: true
       }
     });
+
     this.medium.subscribe("editableInput", this.onChange.bind(this));
   }
 
@@ -25,22 +43,27 @@ export default class Editor extends React.Component {
     if (this.props.onChange === undefined) return;
 
     const htmlContent = this.medium.getContent();
-    this.props.onChange(htmlContent);
+    const markdownContent = markdown.fromHtml(htmlContent);
+    this.props.onChange(markdownContent);
   }
 
   getHtmlSource() {
-    return { __html: this.props.defaultValue };
+    const htmlContent = markdown.toHtml(this.props.defaultValue);
+
+    return { __html: htmlContent };
   }
 
   render() {
     const props = omit(["defaultValue", "onChange"], this.props);
 
     return (
-      <div
-        dangerouslySetInnerHTML={this.getHtmlSource()}
-        ref={el => (this.el = el)}
-        {...props}
-      />
+      <div ref={el => (this.$container = el)}>
+        <div
+          dangerouslySetInnerHTML={this.getHtmlSource()}
+          ref={el => (this.$editor = el)}
+          {...props}
+        />
+      </div>
     );
   }
 }
