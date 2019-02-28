67784e948fb9e4f4429dcef35b6d5797dddb64fc
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-28 10:40:38 +0100

    contrib/src/lib/customAxios: remove skipAuthCheck param

--- packages/contrib/src/lib/customAxios.js
+++ packages/contrib/src/lib/customAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Router from "next/router";
 
-export default function(skipAuthCheck) {
+export default function() {
   const instance = axios.create({
     baseURL: "http://localhost:3200",
     headers: {
@@ -9,28 +9,26 @@ export default function(skipAuthCheck) {
     }
   });
 
-  if (!Boolean(skipAuthCheck)) {
-    instance.interceptors.response.use(
-      response => response,
-      error => {
-        if (error.response !== undefined && error.response.data !== undefined) {
-          const jwt = sessionStorage.getItem("jwt");
+  instance.interceptors.response.use(
+    response => response,
+    error => {
+      if (error.response !== undefined && error.response.data !== undefined) {
+        const jwt = sessionStorage.getItem("jwt");
 
-          if (
-            typeof jwt !== "string" ||
-            jwt.length === 0 ||
-            error.response.data.message === "JWT expired" ||
-            // Malformed JWT case:
-            error.response.data.message.startsWith("JWSError")
-          ) {
-            Router.push("/login");
-          }
+        if (
+          typeof jwt !== "string" ||
+          jwt.length === 0 ||
+          error.response.data.message === "JWT expired" ||
+          // Malformed JWT case:
+          error.response.data.message.startsWith("JWSError")
+        ) {
+          Router.push("/login");
         }
-
-        return error;
       }
-    );
-  }
+
+      return error;
+    }
+  );
 
   return instance;
 }
