0a7a0dcd850e33e8cbd34543beaf9e396f837d32
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-28 14:41:33 +0100

    contrib: remove sockets & related logic

--- packages/contrib/db.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require("pg");
-
-module.exports = async function(dbUri) {
-  const pool = new Pool({ connectionString: dbUri });
-  await pool.connect();
-
-  return pool;
-};
--- packages/contrib/server.js
+++ packages/contrib/server.js
@@ -2,26 +2,19 @@ const http = require("http");
 const Koa = require("koa");
 const next = require("next");
 
-const db = require("./db");
 const routes = require("./routes");
-const sockets = require("./sockets");
 
 const { NODE_ENV, PORT } = process.env;
 const environment = NODE_ENV !== undefined ? NODE_ENV : "development";
-const dbUri = require("../../knexfile")[environment].connection;
 const port = PORT !== undefined ? parseInt(PORT) : 3100;
 
 const nextApp = next({ dev: environment !== "production" });
 
 async function start() {
   await nextApp.prepare();
-  const dbPool = await db(dbUri);
 
   const koaApp = new Koa();
   const server = http.createServer(koaApp.callback());
-
-  // Attach websocket listeners
-  sockets(server, dbPool);
 
   koaApp.use(async (ctx, next) => {
     ctx.res.statusCode = 200;
