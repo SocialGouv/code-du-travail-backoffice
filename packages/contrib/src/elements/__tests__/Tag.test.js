eb4bec5f97a2eb9d25ce185cb7c48151dc8b749f
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-02-28 15:03:30 +0100

    contrib/src/elements/Tag: lint test

--- packages/contrib/src/elements/__tests__/Tag.test.js
+++ packages/contrib/src/elements/__tests__/Tag.test.js
@@ -2,9 +2,10 @@ import React from "react";
 import { render } from "react-testing-library";
 import Tag from "../Tag";
 
-describe("<Tag />", () => {
-  it("should render", () => {
+describe("[Contrib] elements/<Tag />", () => {
+  it("should match snapshot", () => {
     const { container } = render(<Tag />);
+
     expect(container).toMatchSnapshot();
   });
 });
