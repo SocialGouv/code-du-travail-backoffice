4a6a2245cf1da95e3ecde06da480dbfeb10389c1
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-06 01:07:19 +0100

    seeds/00-empty-tables: update

--- seeds/00-empty-tables.js
+++ seeds/00-empty-tables.js
@@ -1,9 +1,19 @@
+const ora = require('ora')
+global.spinner = ora()
+
 exports.seed = async knex => {
-  await knex('api.answers_tags').del()
-  await knex('api.questions_tags').del()
+  global.spinner.start(`Emptying tables...`)
+
+  await knex('answers_tags').del()
+  await knex('questions_tags').del()
+  await knex('api.tags').del()
   await knex('api.answers').del()
   await knex('api.questions').del()
-  await knex('api.tags').del()
-  await knex('api.labor_agreements').del()
-  await knex('basic_auth.users').del()
+  await knex('users_agreements').del()
+  await knex('auth.users').del()
+  await knex('locations_agreements').del()
+  await knex('api.locations').del()
+  await knex('api.agreements').del()
+
+  global.spinner.succeed(`Tables emptied.`)
 }
