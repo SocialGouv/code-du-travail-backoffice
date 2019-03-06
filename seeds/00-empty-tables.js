e24639860f94725aa85b645f1a67415ac19a9943
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-06 15:28:21 +0100

    seeds/00-empty-tables: migrate m2m tables moved into api schema

--- seeds/00-empty-tables.js
+++ seeds/00-empty-tables.js
@@ -4,14 +4,14 @@ global.spinner = ora()
 exports.seed = async knex => {
   global.spinner.start(`Emptying tables...`)
 
-  await knex('answers_tags').del()
-  await knex('questions_tags').del()
+  await knex('api.answers_tags').del()
+  await knex('api.questions_tags').del()
   await knex('api.tags').del()
   await knex('api.answers').del()
   await knex('api.questions').del()
   await knex('users_agreements').del()
   await knex('auth.users').del()
-  await knex('locations_agreements').del()
+  await knex('api.locations_agreements').del()
   await knex('api.locations').del()
   await knex('api.agreements').del()
 
