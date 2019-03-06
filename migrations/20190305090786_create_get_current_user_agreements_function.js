c31c424696539cf9738b047e71d2d546547bb1eb
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-06 15:22:22 +0100

    migrations/20190305090786_create_get_current_user_agreements_function: create

---
+++ migrations/20190305090786_create_get_current_user_agreements_function.js
@@ -0,0 +1,25 @@
+exports.up = async knex => {
+  await knex.raw(`
+    -- Get current authenticated user agreements scope.
+    CREATE FUNCTION
+    get_current_user_agreements() RETURNS uuid[] AS $$
+    DECLARE
+      _current_user_agreements uuid[];
+      _current_user_id uuid := current_setting('request.jwt.claim.id', true)::uuid;
+    BEGIN
+      SELECT array_agg(agreement_id) INTO _current_user_agreements
+      FROM (
+        SELECT agreement_id
+        FROM users_agreements
+        WHERE user_id = _current_user_id
+      ) AS r;
+
+      RETURN _current_user_agreements;
+    END;
+    $$ LANGUAGE plpgsql
+  `);
+}
+
+exports.down = async knex => {
+  await knex.raw('DROP FUNCTION get_current_user_agreements')
+}
