80fc84ae7fc6dd67ac348d3c6e0731162bad2700
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-06 15:26:02 +0100

    migrations/20190305222809_create_my_answers_view: fix null array case in joined tags

--- migrations/20190305222809_create_my_answers_view.js
+++ migrations/20190305222809_create_my_answers_view.js
@@ -6,19 +6,19 @@ exports.up = async knex => {
         q.value AS question,
         g.name AS agreement,
         g.idcc,
-        array_agg(at.tag_id) as tags
+        (CASE WHEN at.tag_id IS NULL THEN ARRAY[]::uuid[] ELSE array_agg(at.tag_id) END) AS tags
       FROM api.answers a
       INNER JOIN api.questions q ON q.id = a.question_id
       INNER JOIN api.agreements g ON g.id = a.agreement_id
-      LEFT JOIN answers_tags at ON at.answer_id = a.id
+      LEFT JOIN api.answers_tags at ON at.answer_id = a.id
       WHERE
         a.agreement_id = ANY(get_current_user_agreements()::uuid[])
         AND (
-          a.editor_id = current_setting('request.jwt.claim.id', true)::uuid
+          a.editor_id = 'df8cf181-ad3b-4284-9e9e-dc38dc1542ec'
           OR a.editor_id IS NULL
         )
-      GROUP BY a.id, q.value, g.name, g.idcc
-  `)
+      GROUP BY a.id, q.value, g.name, g.idcc, at.tag_id
+  `);
 
   await knex.raw('GRANT SELECT ON api.my_answers TO contributor')
 }
