b5001a369a4726777b32b94bc87429cda602d5b0
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-10 17:01:44 +0100

    scripts/db/getMigrationQuery: create

---
+++ scripts/db/getMigrationQuery.js
@@ -0,0 +1,24 @@
+const { readFileSync } = require("fs");
+
+const migrationsPath = `${process.cwd()}/db/migrations`;
+
+class MigrationQuery {
+  constructor(migrationName) {
+    const filePath = `${migrationsPath}/${migrationName}.sql`;
+    const sqlSource = readFileSync(filePath, "utf8");
+
+    this.queries = sqlSource
+      .split(/-+ DOWN -+/)
+      .map((s, i) => i === 0 ? s.replace(/-+ UP -+/, "") : s);
+  }
+
+  up() {
+    return this.queries[0];
+  }
+
+  down() {
+    return this.queries[1];
+  }
+}
+
+module.exports = migrationName => new MigrationQuery(migrationName);
