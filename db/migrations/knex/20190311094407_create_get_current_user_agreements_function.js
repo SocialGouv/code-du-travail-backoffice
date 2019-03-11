9cca7cf1998f0e3dc858d0ebd139771079edb9d2
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-11 13:48:35 +0100

    db/migrations/20190311094407_create_get_current_user_agreements_function: create

---
+++ db/migrations/knex/20190311094407_create_get_current_user_agreements_function.js
@@ -0,0 +1,9 @@
+const getMigrationQuery = require("../../../scripts/db/getMigrationQuery");
+
+exports.up = async knex => {
+  await knex.raw(getMigrationQuery("20190311094407_create_get_current_user_agreements_function").up());
+};
+
+exports.down = async knex => {
+  await knex.raw(getMigrationQuery("20190311094407_create_get_current_user_agreements_function").down());
+};
