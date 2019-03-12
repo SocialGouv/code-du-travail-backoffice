5d748ba91c74d2b477836a89db409b9670f8fa31
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-12 08:47:32 +0100

    contrib/next: add API_URI in publicRuntimeConfig

--- packages/contrib/next.config.js
+++ packages/contrib/next.config.js
@@ -2,6 +2,11 @@ const merge = require("webpack-merge");
 const withCss = require("@zeit/next-css");
 
 module.exports = withCss({
+  // https://github.com/zeit/next.js#runtime-configuration
+  publicRuntimeConfig: {
+    API_URI: `http://localhost:${process.env.API_PORT}`
+  },
+
   webpack: config => ({
     ...config,
     module: merge(config.module, {
