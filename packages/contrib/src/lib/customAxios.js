714f4121754b88dbed38017b349e8e7da197c41a
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-12 15:26:25 +0100

    contrib/src/lib/customAxios: cache instance

--- packages/contrib/src/lib/customAxios.js
+++ packages/contrib/src/lib/customAxios.js
@@ -2,32 +2,36 @@ import axios from "axios";
 import getConfig from "next/config";
 import Router from "next/router";
 
-const API_URI = getConfig().publicRuntimeConfig.API_URI;
+const instance = axios.create({
+  baseURL: getConfig().publicRuntimeConfig.API_URI
+});
+
+instance.interceptors.response.use(
+  response => response,
+  error => {
+    if (
+      error.response !== undefined &&
+      error.response.status !== undefined &&
+      error.response.status === 401
+    ) {
+      Router.push(`/login?redirectTo=${window.location.pathname}`);
+
+      throw undefined;
+    }
+
+    throw error;
+  }
+);
 
 export default function() {
-  const instance = axios.create({
-    baseURL: API_URI,
-    headers: {
-      Authorization: `Bearer ${sessionStorage.getItem("jwt")}`
+  const jwt = sessionStorage.getItem("jwt");
+  if (jwt !== null) {
+    const authorization = `Bearer ${sessionStorage.getItem("jwt")}`;
+
+    if (instance.defaults.headers["Authorization"] !== authorization) {
+      instance.defaults.headers["Authorization"] = authorization;
     }
-  });
-
-  instance.interceptors.response.use(
-    response => response,
-    error => {
-      if (
-        error.response !== undefined &&
-        error.response.status !== undefined &&
-        error.response.status === 401
-      ) {
-        Router.push(`/login?redirectTo=${window.location.pathname}`);
-
-        throw undefined;
-      }
-
-      throw error;
-    }
-  );
+  }
 
   return instance;
 }
