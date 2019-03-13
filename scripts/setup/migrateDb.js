1074a5015ed00c031a3407cd7b81fcf07d2765b3
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-13 16:23:20 +0100

    scripts/setup: migrate auto-setup to new DC structure

--- scripts/setup/migrateDb.js
+++ scripts/setup/migrateDb.js
@@ -1,7 +1,9 @@
-
 module.exports = async rootPath => {
-  const config = require(`${rootPath}/knexfile.js`).development;
-  const knex = require('knex')(config)
-
-  return knex.migrate.latest()
-}
+  try {
+    const config = require(`${rootPath}/knexfile.js`).development;
+    const knex = require("knex")(config);
+    await knex.migrate.latest();
+  } catch (err) {
+    console.log(err);
+  }
+};
