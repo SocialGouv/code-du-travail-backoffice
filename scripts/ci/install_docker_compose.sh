7a6ebbd042204917ebebe7490359fdff3225602e
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-14 15:38:52 +0100

    travis: replace docker-compose installation

--- scripts/ci/install_docker_compose.sh
+++
@@ -1,4 +0,0 @@
-sudo rm /usr/local/bin/docker-compose
-curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
-sudo mv docker-compose /usr/local/bin
