36c4ec74d5624ee2621c767cd6f9fab54fffb2dc
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-15 16:39:56 +0100

    contrib/jest: add MutatinoObserver & document.getSelection() mocks

--- packages/contrib/__tests__/jest.setup.js
+++ packages/contrib/__tests__/jest.setup.js
@@ -1,6 +1,17 @@
-import { setConfig } from "next/config";
+// MutationObserver polyfill
+// https://github.com/megawac/MutationObserver.js
+import "mutationobserver-shim";
 
 // https://github.com/zeit/next.js/issues/4024#issuecomment-386016077
 // TODO Wait for release of: https://github.com/zeit/next.js/pull/6458.
+import { setConfig } from "next/config";
 import nextConfig from "../__mocks__/next.config";
 setConfig(nextConfig);
+
+// Polyfill for document.getSelection()
+// https://gist.github.com/yckart/6435861
+Object.defineProperty(document, "getSelection", {
+  value: () => {
+    return document.selection && document.selection.createRange().text;
+  }
+});
