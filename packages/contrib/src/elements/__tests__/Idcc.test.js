b163267ac5b1de52e203744c6aee7deaa7b8cea4
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-18 17:57:12 +0100

    contrib/src/elements/Idcc.test: ignore console warning

--- packages/contrib/src/elements/__tests__/Idcc.test.js
+++ packages/contrib/src/elements/__tests__/Idcc.test.js
@@ -2,6 +2,9 @@ import React from "react";
 import { render } from "react-testing-library";
 import Idcc from "../Idcc";
 
+// Ignore styled-wrapped ReactTooltip className prop warning
+console.warn = jest.fn();
+
 describe("[Contrib] elements/<Idcc />", () => {
   it("should match snapshot", () => {
     const data = { idcc: "1234", name: "A labor agreement title" };
