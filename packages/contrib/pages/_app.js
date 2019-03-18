6f34ca8c988b01fead9a8ec9ffdc8823d28896f5
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-18 17:07:37 +0100

    contrib/pages/_app: check authentication on first frontend load

--- packages/contrib/pages/_app.js
+++ packages/contrib/pages/_app.js
@@ -4,8 +4,32 @@ import { Provider } from "react-redux";
 
 import withReduxStore from "../src/lib/withReduxStore";
 
+import Main from "../src/layouts/Main";
+import isAuthenticated from "../src/lib/isAuthenticated";
+
 class MainApp extends App {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isMountedAndAllowed: false
+    };
+  }
+
+  async componentDidMount() {
+    if (
+      !window.location.pathname.startsWith("/login") &&
+      !(await isAuthenticated())
+    ) {
+      window.location.href = `/login?redirectTo=${window.location.pathname}`;
+    } else {
+      this.setState({ isMountedAndAllowed: true });
+    }
+  }
+
   render() {
+    if (!this.state.isMountedAndAllowed) return <Main isLoading />;
+
     const { Component, pageProps, reduxStore } = this.props;
 
     return (
