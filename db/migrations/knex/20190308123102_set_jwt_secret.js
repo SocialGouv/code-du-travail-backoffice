58e9f344e84488ccd651aa24e84cc733091776bd
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-18 13:40:45 +0100

    db/migrations: switch to clean JWT secret injection

--- db/migrations/knex/20190308123102_set_jwt_secret.js
+++ db/migrations/knex/20190308123102_set_jwt_secret.js
@@ -1,19 +1,17 @@
 /**
- * TODO Find a better way to pass the JWT secret.
+ * @see http://postgrest.org/en/v5.2/auth.html#jwt-from-sql
  */
+
 exports.up = async knex => {
   await knex.raw(`
-    CREATE FUNCTION
-      getJwtSecret() RETURNS name AS $$
-      BEGIN
-        RETURN '${process.env.PGRST_JWT_SECRET}';
-      END
-      $$ LANGUAGE plpgsql;
+    ALTER DATABASE ${process.env.POSTGRES_DB}
+      SET "app.jwt_secret" TO '${process.env.PGRST_JWT_SECRET}';
   `);
 };
 
 exports.down = async knex => {
   await knex.raw(`
-    DROP FUNCTION getJwtSecret;
+    ALTER DATABASE ${process.env.POSTGRES_DB}
+      RESET "app.jwt_secret";
   `);
 };
