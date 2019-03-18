da896a16fbb0aea73fa2b2d0dd88ac3563fb5b79
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-18 18:08:37 +0100

    contrib/src/libs/isAuthenticated: fix tests

--- packages/contrib/src/libs/__tests__/isAuthenticated.test.js
+++ packages/contrib/src/libs/__tests__/isAuthenticated.test.js
@@ -1,4 +1,3 @@
-import "../../../__mocks__/console";
 import "../../../__mocks__/sessionStorage";
 
 import axios from "axios";
@@ -7,26 +6,37 @@ jest.mock("axios");
 import isAuthenticated from "../isAuthenticated";
 
 describe("[Contrib] lib/isAuthenticated()", () => {
-  axios.get.mockRejectedValueOnce();
-  axios.get.mockResolvedValueOnce();
+  axios.post.mockRejectedValueOnce();
+  axios.post.mockResolvedValueOnce({
+    data: [{ valid: false }]
+  });
+  axios.post.mockResolvedValueOnce({
+    data: [{ valid: true }]
+  });
 
   it("should return false with no JWT in session", async () => {
     expect(sessionStorage.getItem("jwt")).toBe(null);
     expect(await isAuthenticated()).toBe(false);
-    expect(console.warn).toHaveBeenCalledTimes(0);
+    expect(axios.post).toHaveBeenCalledTimes(0);
   });
 
-  it("should return false when /api/answers rejects", async () => {
+  it("should return false when /rpc/login_check rejects", async () => {
     const token = "aFakeToken";
     sessionStorage.setItem("jwt", token);
 
     expect(sessionStorage.getItem("jwt")).toBe(token);
     expect(await isAuthenticated()).toBe(false);
-    expect(console.warn).toHaveBeenCalledTimes(1);
+    expect(axios.post).toHaveBeenCalledTimes(1);
   });
 
-  it("should return true when /api/answers resolves", async () => {
+  // eslint-disable-next-line max-len
+  it("should return false when /rpc/login_check invalidates the JWT", async () => {
+    expect(await isAuthenticated()).toBe(false);
+    expect(axios.post).toHaveBeenCalledTimes(2);
+  });
+
+  it("should return true when /rpc/login_check validates the JWT", async () => {
     expect(await isAuthenticated()).toBe(true);
-    expect(console.warn).toHaveBeenCalledTimes(1);
+    expect(axios.post).toHaveBeenCalledTimes(3);
   });
 });
