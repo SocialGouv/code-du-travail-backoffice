3f41b50a8d762daf20a697ebff4bfe120b43e568
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-19 16:16:33 +0100

    contrib/src/layouts/Header: add user info & logout button

--- packages/contrib/src/layouts/Header.js
+++ packages/contrib/src/layouts/Header.js
@@ -4,6 +4,7 @@ import { Flex } from "rebass";
 import styled from "styled-components";
 
 import marianneImageUri from "../images/marianne.svg";
+import logoutImageUri from "../images/logout.svg";
 
 const Container = styled(Flex)`
   background-color: white;
@@ -11,6 +12,7 @@ const Container = styled(Flex)`
   min-height: 6rem;
   padding: 1rem;
 `;
+
 const Brand = styled(Flex)`
   cursor: pointer;
   height: 3rem;
@@ -31,14 +33,62 @@ const Subtitle = styled.span`
   line-height: 1.5;
 `;
 
-export default () => (
-  <Container alignItems="center" justifyContent="space-between">
-    <Brand alignItems="center" onClick={() => Router.push("/")}>
-      <Logo src={marianneImageUri} alt="Code du travail numérique" />
-      <Flex flexDirection="column">
-        <Title>Code du travail numérique</Title>
-        <Subtitle>Outil de contribution</Subtitle>
-      </Flex>
-    </Brand>
-  </Container>
-);
+const UserInfo = styled(Flex)`
+  margin-right: 1rem;
+`;
+const UserName = styled.span`
+  font-weight: 600;
+  width: auto;
+`;
+const UserLocation = styled.span`
+  color: lightgray;
+  width: auto;
+`;
+const UserLogoutIcon = styled.img`
+  cursor: pointer;
+  height: 1rem;
+  width: 1rem;
+`;
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      me: null
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ me: JSON.parse(sessionStorage.getItem("me")) });
+  }
+
+  logOut() {
+    sessionStorage.removeItem("jwt");
+    sessionStorage.removeItem("me");
+    window.location.reload();
+  }
+
+  render() {
+    return (
+      <Container alignItems="center" justifyContent="space-between">
+        <Brand alignItems="center" onClick={() => Router.push("/")}>
+          <Logo src={marianneImageUri} alt="Code du travail numérique" />
+          <Flex flexDirection="column">
+            <Title>Code du travail numérique</Title>
+            <Subtitle>Outil de contribution</Subtitle>
+          </Flex>
+        </Brand>
+        {this.state.me !== null && (
+          <Flex alignItems="center">
+            <UserInfo alignItems="flex-end" flexDirection="column">
+              <UserName>{this.state.me.payload.name}</UserName>
+              <UserLocation>{this.state.me.payload.location}</UserLocation>
+            </UserInfo>
+            <UserLogoutIcon onClick={this.logOut} src={logoutImageUri} />
+          </Flex>
+        )}
+      </Container>
+    );
+  }
+}
