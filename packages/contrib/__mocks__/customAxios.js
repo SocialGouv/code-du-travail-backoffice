1eedac4850db51905dcd0cd9101802a7327baadd
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-20 10:25:24 +0100

    contrib/__mocks__/customAxios: create

---
+++ packages/contrib/__mocks__/customAxios.js
@@ -0,0 +1,20 @@
+import "../src/libs/customAxios";
+
+jest.mock("../src/libs/customAxios", () => () => {
+  const customAxios = {
+    delete: async () => void 0,
+    get: async () => void 0,
+    options: async () => void 0,
+    patch: async () => void 0,
+    post: async () => void 0,
+    put: async () => void 0
+  };
+
+  jest.spyOn(customAxios, "delete");
+  jest.spyOn(customAxios, "get");
+  jest.spyOn(customAxios, "patch");
+  jest.spyOn(customAxios, "post");
+  jest.spyOn(customAxios, "put");
+
+  return customAxios;
+});
