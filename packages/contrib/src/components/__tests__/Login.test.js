9c0986627e65946654f346bd7ef04c6008c8e0eb
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-20 10:53:37 +0100

    contrib/src/components/Login: reduce tests to snapshot only

--- packages/contrib/src/components/__tests__/Login.test.js
+++ packages/contrib/src/components/__tests__/Login.test.js
@@ -1,65 +1,13 @@
-import "../../../__mocks__/sessionStorage";
-import "../../../__mocks__/waitFor";
-
-import axios from "axios";
-jest.mock("axios");
-
 import React from "react";
-import { fireEvent, render } from "react-testing-library";
+import { render } from "react-testing-library";
+
 import Login from "../Login";
 
 describe("[Contrib] components/<Login />", () => {
   const onLog = jest.fn();
   const { container } = render(<Login onLog={onLog} />);
-  const $button = container.querySelector("button");
-  const $error = container.querySelector("p");
-  const $form = container.querySelector("form");
-  const $input = container.querySelector("input");
-
-  const TOKEN = "aFakeJWT";
-  const res = { data: [{ token: TOKEN }] };
-  axios.post.mockRejectedValueOnce(new Error());
-  axios.post.mockResolvedValueOnce(res);
 
   it("should match snapshot", () => {
     expect(container).toMatchSnapshot();
-  });
-
-  describe("when submitting the form", () => {
-    it("should show the expected error with an empty email input", () => {
-      fireEvent.submit($form);
-
-      expect($error.innerHTML).toBe("Vous devez renseigner votre e-mail.");
-      expect(sessionStorage.getItem("jwt")).toBe(null);
-      expect(onLog.mock.calls.length).toBe(0);
-    });
-
-    it("should show the expected error when /rpc/login rejects", async () => {
-      fireEvent.change($input, { target: { value: "ko@example.com" } });
-      fireEvent.click($button);
-
-      expect(axios.post).toHaveBeenCalledTimes(1);
-      await waitFor(0);
-      expect($error.innerHTML).toBe("E-mail non reconnu.");
-      expect(sessionStorage.getItem("jwt")).toBe(null);
-      expect(onLog.mock.calls.length).toBe(0);
-    });
-
-    it("should not show any error when /rpc/login resolves", async () => {
-      fireEvent.change($input, { target: { value: "ok@example.com" } });
-      fireEvent.click($button);
-
-      expect(axios.post).toHaveBeenCalledTimes(2);
-      await waitFor(0);
-      expect($error.innerHTML).toBe("");
-    });
-
-    it("should have saved JWT in session", () => {
-      expect(sessionStorage.getItem("jwt")).toBe(TOKEN);
-    });
-
-    it("should have called onLog() prop once", () => {
-      expect(onLog.mock.calls.length).toBe(1);
-    });
   });
 });
