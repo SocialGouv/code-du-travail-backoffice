4e60cc4e69c0623a3d416f5dfec2c0bc66602d71
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-21 20:38:02 +0100

    contrib/src/layouts/AdminMain: create

---
+++ packages/contrib/src/layouts/AdminMain.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+import AdminMenu from "./AdminMenu";
+import Main from "./Main";
+
+const Content = styled.div`
+  flex-grow: 1;
+`;
+
+export default ({ children, isLoading }) => (
+  <Main isHorizontal isLoading={isLoading}>
+    <AdminMenu />
+    <Content>{children}</Content>
+  </Main>
+);
