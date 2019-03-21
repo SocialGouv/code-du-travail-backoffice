85e17df7679b7af13f7dd16eefeaf5c926b63475
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-21 20:38:57 +0100

    contrib/routes: add admin routes

--- packages/contrib/routes.js
+++ packages/contrib/routes.js
@@ -15,6 +15,78 @@ module.exports = function(nextApp) {
     ctx.respond = false;
   });
 
+  router.get("/admin/agreements/new", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/agreements/new", {});
+    ctx.respond = false;
+  });
+
+  router.get("/admin/agreements/:id", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/agreements/edit", {
+      ...ctx.params
+    });
+    ctx.respond = false;
+  });
+
+  router.get("/admin/answers/new", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/answers/new", {});
+    ctx.respond = false;
+  });
+
+  router.get("/admin/answers/:id", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/answers/edit", {
+      ...ctx.params
+    });
+    ctx.respond = false;
+  });
+
+  router.get("/admin/locations/new", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/locations/new", {});
+    ctx.respond = false;
+  });
+
+  router.get("/admin/locations/:id", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/locations/edit", {
+      ...ctx.params
+    });
+    ctx.respond = false;
+  });
+
+  router.get("/admin/questions/new", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/questions/new", {});
+    ctx.respond = false;
+  });
+
+  router.get("/admin/questions/:id", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/questions/edit", {
+      ...ctx.params
+    });
+    ctx.respond = false;
+  });
+
+  router.get("/admin/tags/new", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/tags/new", {});
+    ctx.respond = false;
+  });
+
+  router.get("/admin/tags/:id", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/tags/edit", {
+      ...ctx.params
+    });
+    ctx.respond = false;
+  });
+
+  router.get("/admin/users/new", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/users/new", {});
+    ctx.respond = false;
+  });
+
+  router.get("/admin/users/:id", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/users/edit", {
+      ...ctx.params
+    });
+    ctx.respond = false;
+  });
+
   router.get("*", async ctx => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
