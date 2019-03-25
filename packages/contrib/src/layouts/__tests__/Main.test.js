2337fab4b4e0419d650b275cab4bedda1a879206
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-25 21:09:37 +0100

    contrib/layouts/Main: complete tests

--- packages/contrib/src/layouts/__tests__/Main.test.js
+++ packages/contrib/src/layouts/__tests__/Main.test.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { render } from "react-testing-library";
+
 import Main from "../Main";
 
 describe("[Contrib] layouts/<Main />", () => {
-  it("should render", () => {
-    const { container } = render(<Main />);
+  const { asFragment, container } = render(<Main />);
+  const firstRender = asFragment();
 
+  it("should match snapshot", () => {
     expect(container).toMatchSnapshot();
+  });
+
+  it("should show the loading spinner", () => {
+    const { asFragment } = render(<Main isLoading />);
+
+    expect(firstRender).toMatchDiffSnapshot(asFragment());
   });
 });
