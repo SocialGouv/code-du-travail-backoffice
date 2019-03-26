0880da3a7383a9c533fa03257174d552326044ba
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-26 21:04:40 +0100

    contrib/__mocks__/customAxios: attach mocked methods globally

--- packages/contrib/__mocks__/customAxios.js
+++ packages/contrib/__mocks__/customAxios.js
@@ -1,19 +1,13 @@
 import "../src/libs/customAxios";
 
+global.axios = {
+  delete: jest.fn(),
+  get: jest.fn(),
+  patch: jest.fn(),
+  post: jest.fn(),
+  put: jest.fn()
+};
+
 jest.mock("../src/libs/customAxios", () => () => {
-  const customAxios = {
-    delete: async () => void 0,
-    get: async () => void 0,
-    patch: async () => void 0,
-    post: async () => void 0,
-    put: async () => void 0
-  };
-
-  jest.spyOn(customAxios, "delete");
-  jest.spyOn(customAxios, "get");
-  jest.spyOn(customAxios, "patch");
-  jest.spyOn(customAxios, "post");
-  jest.spyOn(customAxios, "put");
-
-  return customAxios;
+  return global.axios;
 });
