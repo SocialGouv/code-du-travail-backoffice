ce5fb4c41f486cb246c4f449974c9ec1bec5954e
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-03-26 20:07:24 +0100

    contrib/elements/SavingSpinner: complete tests

--- packages/contrib/src/elements/__tests__/SavingSpinner.test.js
+++ packages/contrib/src/elements/__tests__/SavingSpinner.test.js
@@ -4,7 +4,19 @@ import SavingSpinner from "../SavingSpinner";
 
 describe("[Contrib] elements/<SavingSpinner />", () => {
   it("should match snapshot", () => {
-    const { container } = render(<SavingSpinner size="8rem" />);
+    const { container } = render(<SavingSpinner />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should match snapshot", () => {
+    const { container } = render(<SavingSpinner color="red" />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should match snapshot", () => {
+    const { container } = render(<SavingSpinner size="32" />);
 
     expect(container).toMatchSnapshot();
   });
