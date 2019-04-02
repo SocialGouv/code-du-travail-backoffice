99f9167635b36a9370a1dd6b9c735fe980951186
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-04-02 13:01:44 +0200

    contrib/elements/Input: create

---
+++ packages/contrib/src/elements/Input.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export default styled.input`
+  -webkit-appearance: none;
+  background-color: transparent;
+  border: solid 1px lightgray;
+  color: inherit;
+  flex-grow: 1;
+  font-family: inherit;
+  font-weight: inherit;
+  font-size: inherit;
+  height: 2rem;
+  line-height: 1;
+  padding: 0.4rem 0.6rem 0.575rem;
+`;
