3f62b5870ef80642495d4f50dbbcfbd75979c14e
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-04-02 12:58:59 +0200

    db/migrations/20190328075853_create_cud_user_functions: fix api.update_user() & api.delete_user() and remove update_user_password()

--- db/migrations/20190328075853_create_cud_user_functions.sql
+++ db/migrations/20190328075853_create_cud_user_functions.sql
@@ -32,63 +32,80 @@ CREATE FUNCTION
     id uuid,
     role name,
     email varchar(255),
+    password varchar(255),
     name varchar(255),
+    location_id uuid,
     agreements uuid[] DEFAULT '{}'::uuid[]
   ) RETURNS void AS $$
   DECLARE
     _agreement_id uuid;
+    _email varchar(255) := email;
+    _password varchar(255) := password;
+    _id uuid := id;
+    _location_id uuid := location_id;
+    _name varchar(255) := name;
+    _role name := role;
   BEGIN
-    UPDATE auth.users
-    SET (role, email, name) = (
-      api.create_user.role,
-      api.create_user.email,
-      api.create_user.name
-    )
-    WHERE id = api.update_user_password.id;
+    -- If the password is not an empty string, we need to update it:
+    IF (LENGTH(_password) > 0) THEN
+      UPDATE auth.users
+      SET (role, email, password, name, location_id) =(
+        _role,
+        _email,
+        _password,
+        _name,
+        _location_id
+      )
+      WHERE auth.users.id = _id;
+    ELSE
+      UPDATE auth.users
+      SET (role, email, name, location_id) =(
+        _role,
+        _email,
+        _name,
+        _location_id
+      )
+      WHERE auth.users.id = _id;
+    END IF;
 
     -- Insert new agreements which do not already exist:
     FOREACH _agreement_id IN ARRAY agreements
     LOOP
       IF NOT EXISTS (
         SELECT * FROM users_agreements
-        WHERE user_id = id
+        WHERE user_id = _id
           AND agreement_id = _agreement_id
       ) THEN
         INSERT INTO users_agreements (user_id, agreement_id)
-        VALUES (id, _agreement_id);
+        VALUES (_id, _agreement_id);
       END IF;
     END LOOP;
 
     -- Delete existing agreements which are not part of the agreements array:
     DELETE FROM users_agreements
-    WHERE user_id = id
-      AND agreement_id != ANY(agreements);
+    WHERE user_id = _id
+      AND NOT (agreement_id = ANY(agreements));
   END
   $$ LANGUAGE plpgsql;
 
 GRANT EXECUTE ON FUNCTION api.update_user TO administrator;
 
 CREATE FUNCTION
-  api.update_user_password(id uuid, password varchar(255)) RETURNS void AS $$
-  BEGIN
-    UPDATE auth.users
-    SET password = api.update_user_password.password
-    WHERE id = api.update_user_password.id;
-  END
-  $$ LANGUAGE plpgsql;
-
-GRANT EXECUTE ON FUNCTION api.update_user_password TO administrator;
-
-CREATE FUNCTION
   api.delete_user(id uuid) RETURNS void AS $$
+  DECLARE
+    _id uuid := id;
   BEGIN
     -- Unlink all the user current answers:
     UPDATE api.answers
     SET user_id = NULL
-    WHERE user_id = id;
+    WHERE user_id = _id;
+
+    -- Delete the allowed agreements for this user:
+    DELETE FROM users_agreements
+    WHERE user_id = _id;
 
     DELETE FROM auth.users
-    WHERE id = api.delete_user.id;
+    WHERE auth.users.id = _id;
   END
   $$ LANGUAGE plpgsql;
 
@@ -97,7 +114,6 @@ GRANT EXECUTE ON FUNCTION api.delete_user TO administrator;
 ------------------------------------- DOWN -------------------------------------
 
 DROP FUNCTION api.delete_user;
-DROP FUNCTION api.update_user_password;
 DROP FUNCTION api.update_user;
 DROP FUNCTION api.create_user;
 
