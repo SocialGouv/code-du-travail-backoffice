dc8ce54b821e0d31838959f3b5fbe28f40c165cf
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-04-03 18:01:58 +0200

    contrib/elements/Title: add isSecondary prop

--- packages/contrib/src/elements/Title.js
+++ packages/contrib/src/elements/Title.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export default styled.p`
   color: black;
-  font-size: 1.25rem;
+  font-size: ${props => (Boolean(props.isSecondary) ? "1.1" : "1.25")}rem;
   font-weight: 600;
   margin: ${props => (props.isFirst ? "0 0 0.5rem" : "1.5rem 0 0.5rem")};
   user-select: none;
