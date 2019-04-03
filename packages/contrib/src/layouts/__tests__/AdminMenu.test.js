b10dba5391af5883e6df203d1d6807b9809fb2d8
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-04-03 18:01:23 +0200

    contrib/layouts/AdminMenu: add current item highlighting

--- packages/contrib/src/layouts/__tests__/AdminMenu.test.js
+++ packages/contrib/src/layouts/__tests__/AdminMenu.test.js
@@ -7,7 +7,11 @@ jest.mock("next/router");
 import AdminMenu from "../AdminMenu";
 
 describe("[Contrib] layouts/<AdminMenu />", () => {
-  const { container, getByText } = render(<AdminMenu />);
+  // https://github.com/facebook/jest/issues/890#issuecomment-415202799
+  window.history.pushState({}, "", "/admin");
+
+  const { asFragment, container, getByText } = render(<AdminMenu />);
+  const firstRender = asFragment();
 
   it("should match snapshot", () => {
     expect(container).toMatchSnapshot();
@@ -15,31 +19,45 @@ describe("[Contrib] layouts/<AdminMenu />", () => {
 
   it("should redirect to the expected path", () => {
     fireEvent.click(getByText(/Tableau de bord/));
+
     expect(Router.push).toBeCalledWith("/admin");
   });
 
   it("should redirect to the expected path", () => {
     fireEvent.click(getByText(/Conventions/));
+
     expect(Router.push).toBeCalledWith("/admin/agreements");
   });
 
   it("should redirect to the expected path", () => {
     fireEvent.click(getByText(/Étiquettes/));
+
     expect(Router.push).toBeCalledWith("/admin/tags");
   });
 
   it("should redirect to the expected path", () => {
     fireEvent.click(getByText(/Questions/));
+
     expect(Router.push).toBeCalledWith("/admin/questions");
   });
 
   it("should redirect to the expected path", () => {
     fireEvent.click(getByText(/Unités/));
+
     expect(Router.push).toBeCalledWith("/admin/locations");
   });
 
   it("should redirect to the expected path", () => {
     fireEvent.click(getByText(/Utilisateurs/));
+
     expect(Router.push).toBeCalledWith("/admin/users");
+  });
+
+  it("should match snapshot diff when the path has changed", () => {
+    window.history.pushState({}, "", "/admin/agreements");
+
+    const { asFragment } = render(<AdminMenu />);
+
+    expect(firstRender).toMatchDiffSnapshot(asFragment());
   });
 });
