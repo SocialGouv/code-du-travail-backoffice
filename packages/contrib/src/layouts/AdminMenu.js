1169bbd175879aee2d5c7de08101e3dfb7d0ba8e
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-04-03 18:01:23 +0200

    contrib/layouts/AdminMenu: add current item highlighting

--- packages/contrib/src/layouts/AdminMenu.js
+++ packages/contrib/src/layouts/AdminMenu.js
@@ -4,19 +4,23 @@ import { Flex } from "rebass";
 import styled from "styled-components";
 
 const Container = styled(Flex)`
-  background-color: #315659;
+  background-color: var(--color-dark-slate-gray);
   min-width: 12rem;
   width: 12rem;
 `;
 
 const Link = styled.div`
+  background-color: ${props =>
+    Boolean(props.isCurrent) ? "black" : "transparent"};
   border-bottom: 1px solid rgba(255, 255, 255, 0.1);
-  color: rgba(255, 255, 255, 0.75);
+  color: ${props =>
+    Boolean(props.isCurrent) ? "white" : "rgba(255, 255, 255, 0.75)"};
   cursor: pointer;
   padding: 0.5rem;
 
   :hover {
-    background-color: rgba(0, 0, 0, 0.1);
+    background-color: ${props =>
+      Boolean(props.isCurrent) ? "black" : "var(--color-japenese-indigo)"};
     color: white;
   }
 `;
@@ -26,15 +30,48 @@ export default class AdminMenu extends React.PureComponent {
     Router.push(`/admin${resource}`);
   }
 
+  isCurrent(path) {
+    return path.length !== 0
+      ? window.location.pathname.startsWith(`/admin${path}`)
+      : window.location.pathname === "/admin";
+  }
+
   render() {
     return (
       <Container flexDirection="column">
-        <Link onClick={() => this.goTo("")}>Tableau de bord</Link>
-        <Link onClick={() => this.goTo("/agreements")}>Conventions</Link>
-        <Link onClick={() => this.goTo("/tags")}>Étiquettes</Link>
-        <Link onClick={() => this.goTo("/questions")}>Questions</Link>
-        <Link onClick={() => this.goTo("/locations")}>Unités</Link>
-        <Link onClick={() => this.goTo("/users")}>Utilisateurs</Link>
+        <Link isCurrent={this.isCurrent("")} onClick={() => this.goTo("")}>
+          Tableau de bord
+        </Link>
+        <Link
+          isCurrent={this.isCurrent("/agreements")}
+          onClick={() => this.goTo("/agreements")}
+        >
+          Conventions
+        </Link>
+        <Link
+          isCurrent={this.isCurrent("/tags")}
+          onClick={() => this.goTo("/tags")}
+        >
+          Étiquettes
+        </Link>
+        <Link
+          isCurrent={this.isCurrent("/questions")}
+          onClick={() => this.goTo("/questions")}
+        >
+          Questions
+        </Link>
+        <Link
+          isCurrent={this.isCurrent("/locations")}
+          onClick={() => this.goTo("/locations")}
+        >
+          Unités
+        </Link>
+        <Link
+          isCurrent={this.isCurrent("/users")}
+          onClick={() => this.goTo("/users")}
+        >
+          Utilisateurs
+        </Link>
       </Container>
     );
   }
