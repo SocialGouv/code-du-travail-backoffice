80ce50f6be4fd934d6bf65607edb01281b67d1fe
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-04-04 14:56:32 +0200

    contrib/pages/admin/questions/new: create

--- packages/contrib/pages/admin/questions/new.js
+++ packages/contrib/pages/admin/questions/new.js
@@ -1,70 +1,71 @@
-import axios from "axios";
 import React from "react";
-import { Button as ReButton } from "rebass";
-import styled from "styled-components";
 
-import Main from "../../../src/layouts/Main";
+import AdminForm from "../../../src/components/AdminForm";
+import AdminMain from "../../../src/layouts/AdminMain";
+import customAxios from "../../../src/libs/customAxios";
 
-const Form = styled.form`
-  /* box-sizing: border-box;
-  display: flex;
-  flex-direction: row;
-  flex-grow: 1; */
-`;
+const FIELDS = [
+  {
+    type: "text",
+    name: "value",
+    label: "Intitulé"
+  }
+];
 
-const Textarea = styled.textarea`
-  background-color: #315659;
-  border: 0;
-  color: #ffffff;
-  flex-grow: 1;
-  font-family: inherit;
-  font-weight: 600;
-  font-size: 1.25rem;
-  line-height: 1.5;
-  padding: 0.45rem 0.9rem;
-`;
-
-const Button = styled(ReButton)`
-  background-color: #2978a0;
-  border-radius: 0;
-  cursor: pointer;
-`;
-
-export default class extends React.Component {
+export default class AdminQuestionsNewPage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      value: ""
+      fields: [],
+      isLoading: true
     };
-
-    this.updateValue = this.updateValue.bind(this);
   }
 
-  updateValue(event) {
-    this.setState({ value: event.target.value });
-  }
+  async componentDidMount() {
+    this.axios = customAxios();
 
-  save() {
-    const uri = `http://localhost:3200/answers?id=eq.${this.props.id}`;
-    const data = {
-      value: this.value
-    };
-    const config = {
-      headers: { Authorization: `Bearer ${sessionStorage.getItem("jwt")}` }
-    };
+    try {
+      const { data: tags } = await this.axios.get("/tags");
 
-    axios.patch(uri, data, config).catch(console.warn);
+      const fields = [
+        ...FIELDS,
+        {
+          type: "tags",
+          name: "tags",
+          label: "Étiquettes",
+          tags: tags.map(({ id, value }) => ({ id, value })),
+          ariaName: "l'étiquette",
+          apiPath: "/questions_tags",
+          singleName: "tag"
+        }
+      ];
+
+      this.setState({
+        fields,
+        isLoading: false
+      });
+    } catch (err) {
+      if (err !== undefined) console.warn(err);
+    }
   }
 
   render() {
+    if (this.state.isLoading) return <AdminMain isLoading />;
+
     return (
-      <Main>
-        <Form flexDirection="row" onSubmit={() => void 0}>
-          <Textarea onChange={this.updateValue} />
-          <Button>Créer une nouvelle question</Button>
-        </Form>
-      </Main>
+      <AdminForm
+        apiPath="/questions"
+        ariaLabels={{
+          cancelButton: `Bouton redirigeant vers la liste des questions`,
+          createOrEditButton: `Bouton créant une nouvelle question dans la base
+                              de données à partir des données du formulaire`
+        }}
+        fields={this.state.fields}
+        indexPath="/questions"
+        name="question"
+        title="Nouvelle question"
+      />
     );
   }
 }
