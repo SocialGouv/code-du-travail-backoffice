3e563ea407841fb8c5b029bed76fe9130983819b
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-04-04 14:57:58 +0200

    contrib/pages/admin/questions/index: create

--- packages/contrib/pages/admin/questions/index.js
+++ packages/contrib/pages/admin/questions/index.js
@@ -13,7 +13,19 @@ const COLUMNS = [
 
 export default () => (
   <AdminIndex
-    apiUri="/questions"
+    apiPath="/questions"
+    ariaLabels={{
+      cancelDeletionButton: `Bouton annulant la suppression de cette question
+                            de la base de données`,
+      deleteButton: `Bouton confirmant la suppression de cette question de la
+                    base de données`,
+      editButton: `Bouton redirigeant vers le formulaire d'édition des données
+                  de cette question`,
+      newButton: `Bouton redirigeant vers le formulaire de création d'une
+                 nouvelle question`,
+      removeButton: `Bouton supprimant cette question de la base de données
+                    après confirmation`
+    }}
     columns={COLUMNS}
     slug="question"
     title="Questions"
