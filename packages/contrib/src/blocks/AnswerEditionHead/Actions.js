f0466184d91891e43aa73be50879ae6d5e0cd186
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-04-14 18:45:51 +0200

    contrib/blocks/AnswerEditionHead/Actions: create

---
+++ packages/contrib/src/blocks/AnswerEditionHead/Actions.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Flex } from "rebass";
+import styled from "styled-components";
+
+import Button from "../../elements/Button";
+
+const Container = styled(Flex)`
+  margin-top: 0.5rem;
+`;
+
+export default ({ onCancel }) => (
+  <Container>
+    <Button color="secondary" onClick={onCancel}>
+      Annuler le brouillon
+    </Button>
+  </Container>
+);
