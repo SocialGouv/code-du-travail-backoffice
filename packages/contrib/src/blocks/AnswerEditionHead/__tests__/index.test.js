f74831603f555c4d149f4ccda51ce48cb51735fb
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-04-14 18:47:11 +0200

    contrib/blocks/AnswerEditionHead: integrate Actions

--- packages/contrib/src/blocks/AnswerEditionHead/__tests__/index.test.js
+++ packages/contrib/src/blocks/AnswerEditionHead/__tests__/index.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-testing-library";
 
 import AnswerEditionHead from "..";
-import { TABS } from "../Menu";
+import { TABS } from "../Tabs";
 
 describe("[Contrib] blocks/<AnswerEditionHead />", () => {
   // Ignore styled-wrapped ReactTooltip className prop warning
--- packages/contrib/src/blocks/AnswerEditionHead/index.js
+++ packages/contrib/src/blocks/AnswerEditionHead/index.js
@@ -2,18 +2,29 @@ import React from "react";
 import { Flex } from "rebass";
 import styled from "styled-components";
 
-import BaseTitle from "../../elements/Title";
-import Menu from "./Menu";
+import _Title from "../../elements/Title";
+import Actions from "./Actions";
+import Tabs from "./Tabs";
 import Top from "./Top";
 
-const Title = styled(BaseTitle)`
+const Title = styled(_Title)`
   padding: 0 1rem;
 `;
 
-export default ({ agreement, currentTab, idcc, onTabChange, title }) => (
+export default ({
+  agreement,
+  currentTab,
+  idcc,
+  onCancel,
+  onTabChange,
+  title
+}) => (
   <Flex flexDirection="column">
     <Top agreement={agreement} idcc={idcc} />
     <Title isFirst>{title}</Title>
-    <Menu currentTab={currentTab} onTabChange={onTabChange} />
+    <Flex justifyContent="space-between">
+      <Tabs currentTab={currentTab} onChange={onTabChange} />
+      <Actions onCancel={onCancel} />
+    </Flex>
   </Flex>
 );
