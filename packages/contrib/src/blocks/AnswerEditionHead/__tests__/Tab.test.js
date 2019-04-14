3b4aac6fe66b39eda86180cd574318f8e0c2f77b
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-04-14 18:42:06 +0200

    contrib/blocks/AnswerEditionHead/Tab: create

---
+++ packages/contrib/src/blocks/AnswerEditionHead/__tests__/Tab.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { fireEvent, render } from "react-testing-library";
+
+import Tab from "../Tab";
+
+describe("[Contrib] blocks/AnswerEditionHead/<Tab />", () => {
+  const props = {
+    children: "A Tab Text",
+    icon: "book",
+    isActive: false,
+    onClick: jest.fn()
+  };
+
+  const { asFragment, container, getByText } = render(<Tab {...props} />);
+  const firstRender = asFragment();
+
+  it("should match snapshot", () => {
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should trigger onClick() with the expected param", () => {
+    fireEvent.click(getByText(props.children));
+
+    expect(props.onClick).toHaveBeenCalled();
+  });
+
+  it("should match snapshot diff with `isActive` prop", () => {
+    const { asFragment } = render(<Tab {...props} isActive />);
+
+    expect(firstRender).toMatchDiffSnapshot(asFragment());
+  });
+});
