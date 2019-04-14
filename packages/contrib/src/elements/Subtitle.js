e658d8563b84ef03a6fafb9bdc19bf05703064ec
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2019-04-14 19:04:39 +0200

    ci(gitlab): setup first e2e tests with bdd (#72)
    * scripts/prod/start: fix db migration by disabling TTY
    * Dockerfile: fix missing env vars in master image
    * env: add WEB_URI variable
    * docker: create test image (for e2e tests via CI)
    * docker-compose: add test service (for e2e tests)
    * gitlab-ci: add "E2E Tests" job
    * scripts/prod/start: add sleep hack for db image starting
    * contrib/elements/Title: switch to h1 & remove isSecondary prop
    * contrib/elements/Subtitle: create
    * contrib: integrate new Title & Subtitle elements
    * yarn: add chai
    * features: add administrator & contributor login behaviors

--- packages/contrib/src/elements/Subtitle.js
+++ packages/contrib/src/elements/Subtitle.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
-export default styled.h1`
+export default styled.h2`
   color: black;
-  font-size: 1.25rem;
+  font-size: 1.1rem;
   font-weight: 600;
   margin: ${props => (props.isFirst ? "0 0 0.5rem" : "1.5rem 0 0.5rem")};
   user-select: none;
--- packages/contrib/src/elements/__tests__/Subitle.test.js
+++ packages/contrib/src/elements/__tests__/Subitle.test.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { render } from "react-testing-library";
-import Title from "../Title";
+import Subtitle from "../Subtitle";
 
-describe("[Contrib] elements/<Title />", () => {
+describe("[Contrib] elements/<Subtitle />", () => {
   it("should match snapshot", () => {
-    const { container } = render(<Title />);
+    const { container } = render(<Subtitle />);
 
     expect(container).toMatchSnapshot();
   });
 
   it("should match snapshot with `isFirst` prop", () => {
-    const { container } = render(<Title isFirst />);
+    const { container } = render(<Subtitle isFirst />);
 
     expect(container).toMatchSnapshot();
   });
