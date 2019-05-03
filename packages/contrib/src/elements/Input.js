8bc5c36ad6720fc5972b657148a05e729060633d
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-05-03 12:27:10 +0200

    contrib/elements/Input: fix padding

--- packages/contrib/src/elements/Input.js
+++ packages/contrib/src/elements/Input.js
@@ -14,7 +14,7 @@ const _Input = styled.input`
   font-size: 0.875rem;
   height: 2rem;
   line-height: 1;
-  padding: 0.4rem 0.6rem 0.575rem
+  padding: 0.5rem 0.6rem 0.55rem
     ${({ hasIcon }) => (Boolean(hasIcon) ? "2rem" : "0.6rem")};
   width: 100%;
 
