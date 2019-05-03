d7434bd41ec0a4a7d08d55cf2ea605d4e5559b35
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-05-03 10:39:35 +0200

    contrib/elements/Link: create

---
+++ packages/contrib/src/elements/Link.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default styled.button`
+  background: transparent;
+  border: 0;
+  color: var(--color-text-blue);
+  cursor: pointer;
+  padding: 0;
+  text-decoration: underline;
+`;
