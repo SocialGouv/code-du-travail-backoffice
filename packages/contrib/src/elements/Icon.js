cfec2d7ea6e5bb1cfee6d4b99dfef9c88db8abbb
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-05-03 07:20:01 +0200

    contrib/elements/Icon: fix isSmall prop style

--- packages/contrib/src/elements/Icon.js
+++ packages/contrib/src/elements/Icon.js
@@ -2,6 +2,11 @@ import * as Icons from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import styled from "styled-components";
+
+const Icon = styled(FontAwesomeIcon)`
+  width: ${({ size }) => (size === "sm" ? "0.5rem" : "1rem")} !important;
+`;
 
 export default ({
   color = "var(--color-black-leather-jacket)",
@@ -13,11 +18,6 @@ export default ({
   library.add(Icons[`fa${icon[0].toUpperCase()}${icon.substr(1)}`]);
 
   return (
-    <FontAwesomeIcon
-      icon={icon}
-      color={color}
-      size={isSmall ? "sm" : "1x"}
-      {...props}
-    />
+    <Icon icon={icon} color={color} size={isSmall ? "sm" : "1x"} {...props} />
   );
 };
