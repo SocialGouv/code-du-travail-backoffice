1ee6c72e669a4d43848e1bc4d1f237e51a302bd4
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-05-03 10:41:11 +0200

    contrib/elements/Subtitle: update design

--- packages/contrib/src/elements/Subtitle.js
+++ packages/contrib/src/elements/Subtitle.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export default styled.h2`
   color: black;
-  font-size: 1.1rem;
+  font-size: 1.2rem;
   font-weight: 600;
   margin: ${props => (props.isFirst ? "0 0 0.5rem" : "1.5rem 0 0.5rem")};
   user-select: none;
