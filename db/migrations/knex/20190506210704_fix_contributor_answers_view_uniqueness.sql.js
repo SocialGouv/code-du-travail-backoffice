fac819639c7cc56e0789a99ca8808d98c37253ef
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-05-06 21:42:56 +0200

    db/migrations/20190506210704_fix_contributor_answers_view_uniqueness.sql: create

---
+++ db/migrations/knex/20190506210704_fix_contributor_answers_view_uniqueness.sql.js
@@ -0,0 +1,9 @@
+const getMigrationQuery = require("../../../scripts/db/getMigrationQuery");
+
+exports.up = async knex => {
+  await knex.raw(getMigrationQuery("20190506210704_fix_contributor_answers_view_uniqueness.sql").up());
+};
+
+exports.down = async knex => {
+  await knex.raw(getMigrationQuery("20190506210704_fix_contributor_answers_view_uniqueness.sql").down());
+};
