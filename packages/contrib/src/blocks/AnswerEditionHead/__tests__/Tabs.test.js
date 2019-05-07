e20a8bdd34a9b6f75f38a569de44180dfc08af1e
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-05-07 09:33:49 +0200

    contrib/blocks/AnswerEditionHead/Tabs: add tags tab

--- packages/contrib/src/blocks/AnswerEditionHead/__tests__/Tabs.test.js
+++ packages/contrib/src/blocks/AnswerEditionHead/__tests__/Tabs.test.js
@@ -6,7 +6,9 @@ import Tabs, { TABS } from "../Tabs";
 describe("[Contrib] blocks/AnswerEditionHead/<Tabs />", () => {
   const props = {
     currentTab: TABS.EDITOR,
-    onChange: jest.fn()
+    onChange: jest.fn(),
+    referencesCount: 2,
+    tagsCount: 12
   };
 
   const { container, getByText } = render(<Tabs {...props} />);
@@ -15,15 +17,21 @@ describe("[Contrib] blocks/AnswerEditionHead/<Tabs />", () => {
     expect(container).toMatchSnapshot();
   });
 
-  it("should trigger onChange() with the expected param", () => {
-    fireEvent.click(getByText(/Édition du contenu/));
+  it("should trigger onChange() with the expected param (0)", () => {
+    fireEvent.click(getByText("Édition"));
 
     expect(props.onChange).toHaveBeenCalledWith(TABS.EDITOR);
   });
 
-  it("should trigger onChange() with the expected param", () => {
-    fireEvent.click(getByText(/Références juridiques/));
+  it("should trigger onChange() with the expected param (1)", () => {
+    fireEvent.click(getByText(/Références juridiques \(\d+\)/));
 
     expect(props.onChange).toHaveBeenCalledWith(TABS.REFERENCES);
+  });
+
+  it("should trigger onChange() with the expected param (2)", () => {
+    fireEvent.click(getByText(/Étiquettes \(\d+\)/));
+
+    expect(props.onChange).toHaveBeenCalledWith(TABS.TAGS);
   });
 });
