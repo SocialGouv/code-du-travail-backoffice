56eae4d55f55a36ad31f20aa853a529b07210d40
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-05-07 09:30:42 +0200

    contrib/blocks/AnswerEditionContent: remove tags sidebar

--- packages/contrib/src/blocks/AnswerEditionContent/index.js
+++ packages/contrib/src/blocks/AnswerEditionContent/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Flex } from "rebass";
 import styled from "styled-components";
 
-import BaseTagsBar from "./TagsBar";
 import BaseEditor from "../../components/Editor";
 
 const Container = styled(Flex)`
@@ -11,40 +10,9 @@ const Container = styled(Flex)`
 const Editor = styled(BaseEditor)`
   flex-grow: 1;
 `;
-const TagsBar = styled(BaseTagsBar)`
-  max-width: 22rem;
-`;
 
-export default class extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isTagsBarOpen: false
-    };
-  }
-
-  toggleTagsBar() {
-    this.setState({ isTagsBarOpen: !this.state.isTagsBarOpen });
-  }
-
-  render() {
-    return (
-      <Container>
-        <Editor
-          defaultValue={this.props.defaultValue}
-          onChange={this.props.onChange}
-          onTagsClicked={() => this.toggleTagsBar()}
-        />
-        {this.state.isTagsBarOpen && (
-          <TagsBar
-            flexDirection="column"
-            onToggleTag={this.props.onToggleTag}
-            selectedTags={this.props.selectedTags}
-            tags={this.props.tags}
-          />
-        )}
-      </Container>
-    );
-  }
-}
+export default ({ defaultValue, onChange }) => (
+  <Container>
+    <Editor defaultValue={defaultValue} onChange={onChange} />
+  </Container>
+);
