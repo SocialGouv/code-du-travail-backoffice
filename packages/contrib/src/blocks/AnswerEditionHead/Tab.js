4bbf586e66d6ecf8eb43a81402b7788b3c05e779
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-05-07 09:33:28 +0200

    contrib/blocks/AnswerEditionHead/Tab: update design

--- packages/contrib/src/blocks/AnswerEditionHead/Tab.js
+++ packages/contrib/src/blocks/AnswerEditionHead/Tab.js
@@ -2,25 +2,27 @@ import React from "react";
 import { Flex } from "rebass";
 import styled from "styled-components";
 
-import _Icon from "../../elements/Icon";
-
 const Container = styled(Flex)`
-  background-color: ${props => (props.isActive ? "white" : "lightgray")};
-  border-top-left-radius: 0.25rem;
-  border-top-right-radius: 0.25rem;
-  border-right: solid 1px gray;
-  border-top: solid 1px gray;
+  border-bottom: ${props =>
+    props.isActive ? "solid 0.25rem var(--color-blue-sapphire)" : 0};
+  color: ${props =>
+    props.isActive
+      ? "var(--color-lapis-lazuli)"
+      : "var(--color-black-leather-jacket)"};
   cursor: pointer;
-  padding: 0.25rem 0.5rem;
+  font-size: 0.875rem;
+  font-weight: ${props => (props.isActive ? 600 : 400)};
+  padding: 0.5rem 0;
+  width: 12rem;
 `;
 
-const Icon = styled(_Icon)`
-  margin-right: 0.5rem;
-`;
-
-export default ({ children, icon, isActive, onClick }) => (
-  <Container alignItems="center" isActive={isActive} onClick={onClick}>
-    <Icon icon={icon} isSmall />
+export default ({ children, isActive, onClick }) => (
+  <Container
+    alignItems="center"
+    isActive={isActive}
+    justifyContent="center"
+    onClick={onClick}
+  >
     {children}
   </Container>
 );
