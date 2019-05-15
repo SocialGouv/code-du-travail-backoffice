dbfd2daa218b8517dc31ab8dd5e48cd4a522f8d9
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-05-15 09:24:27 +0200

    contrib/constants: create

---
+++ packages/contrib/src/constants.js
@@ -0,0 +1,12 @@
+export const ANSWER_STATE = {
+  TODO: "todo",
+  DRAFT: "draft",
+  PENDING_REVIEW: "pending_review",
+  VALIDATED: "validated"
+};
+export const ANSWER_STATE_LABEL = {
+  todo: "À rédiger",
+  draft: "Brouillon",
+  pending_review: "En attente de validation",
+  validated: "Validée"
+};
