831fa4411e3b4b9837eb8a1ca34391775140df3c
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-05-22 14:33:18 +0200

    contrib/pages/admin/zones/index: create

---
+++ packages/contrib/pages/admin/zones/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import AdminIndex from "../../../src/components/AdminIndex";
+
+import { ZONE_CATEGORY_LABEL } from "../../../src/constants";
+
+import "../../../node_modules/react-table/react-table.css";
+
+const COLUMNS = [
+  {
+    Header: "Code",
+    accessor: "code",
+    width: 80
+  },
+  {
+    Header: "Nom",
+    accessor: "name"
+  },
+  {
+    Header: "Catégorie",
+    accessor: ({ category }) => ZONE_CATEGORY_LABEL[category],
+    id: "category",
+    width: 240
+  }
+];
+
+export default () => (
+  <AdminIndex
+    apiPath="/zones"
+    ariaLabels={{
+      editButton: `Bouton redirigeant vers le formulaire d'édition des données
+                  de cette zone`,
+      newButton: `Bouton redirigeant vers le formulaire de création d'une
+                 nouvelle zone`
+    }}
+    columns={COLUMNS}
+    noDelete
+    slug="zone"
+    title="Zones"
+  />
+);
