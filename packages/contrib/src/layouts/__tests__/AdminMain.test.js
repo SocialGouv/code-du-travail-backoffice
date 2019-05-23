cbcbf20a99da143456075980dc2b62559d036bf2
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-05-23 12:26:05 +0200

    contrib/layouts/AdminMain: complete tests

--- packages/contrib/src/layouts/__tests__/AdminMain.test.js
+++ packages/contrib/src/layouts/__tests__/AdminMain.test.js
@@ -1,11 +1,45 @@
 import React from "react";
 import { render } from "react-testing-library";
+
+jest.mock("next/router", () => ({
+  withRouter: component => {
+    component.defaultProps = {
+      ...component.defaultProps,
+      router: {
+        pathname: "/admin"
+      }
+    };
+
+    return component;
+  }
+}));
+
 import AdminMain from "../AdminMain";
 
-describe.skip("[Contrib] layouts/<AdminMain />", () => {
-  it("should render", () => {
-    const { container } = render(<AdminMain />);
+describe("[Contrib] layouts/<AdminMain />", () => {
+  // https://github.com/facebook/jest/issues/890#issuecomment-415202799
+  window.history.pushState({}, "", "/admin");
 
-    expect(container).toMatchSnapshot();
+  const props = {
+    children: "Some Content Text",
+    isLoading: false
+  };
+
+  const γ = render(<AdminMain {...props} />);
+  const firstRender = γ.container;
+
+  it("should match snapshot", () => {
+    expect(firstRender).toMatchSnapshot();
+  });
+
+  it("should match snapshot diff with `isLoading` prop", () => {
+    const newProps = {
+      ...props,
+      isLoading: true
+    };
+
+    const Γ = render(<AdminMain {...newProps} />);
+
+    expect(firstRender).toMatchDiffSnapshot(Γ.asFragment());
   });
 });
