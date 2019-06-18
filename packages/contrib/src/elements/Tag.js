31de64db18f36fe74628ef9d33a668050fb29f96
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-06-18 16:35:49 +0200

    contrib/elements/Tag: add isDisabled prop

--- packages/contrib/src/elements/Tag.js
+++ packages/contrib/src/elements/Tag.js
@@ -3,13 +3,14 @@ import styled from "styled-components";
 export default styled.span`
   background-color: ${props =>
     props.selected ? "var(--color-lapis-lazuli)" : "transparent"};
-  border: solid 1px var(--color-lapis-lazuli);
   border-radius: 0.8rem;
+  border: solid 1px var(--color-lapis-lazuli);
   color: ${props => (props.selected ? "white" : "var(--color-lapis-lazuli)")};
   cursor: pointer;
   font-size: 0.8rem;
-  white-space: nowrap;
   margin: 0 0.5rem 0.5rem 0;
+  opacity: ${({ isDisabled }) => (Boolean(isDisabled) ? 0.25 : 1)};
   padding: 0.25rem 0.75rem;
   user-select: none;
+  white-space: nowrap;
 `;
