e830e6bdef754cac50f1cb8a8b88f3b787024981
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-07-08 14:14:06 +0200

    contrib/libs/isAuthenticated: fix tests

--- packages/contrib/src/libs/__tests__/isAuthenticated.test.js
+++ packages/contrib/src/libs/__tests__/isAuthenticated.test.js
@@ -3,6 +3,9 @@ import axios from "axios";
 import isAuthenticated from "../isAuthenticated";
 
 describe("[Contrib] lib/isAuthenticated()", () => {
+  const JWT = "aFakeToken";
+  const ME = JSON.stringify({ payload: { agreements: [] } });
+
   axios.post.mockRejectedValueOnce();
   axios.post.mockResolvedValueOnce({
     data: [{ valid: false }]
@@ -11,17 +14,35 @@ describe("[Contrib] lib/isAuthenticated()", () => {
     data: [{ valid: true }]
   });
 
-  it("should return false with no JWT in session", async () => {
+  it("should return false with no `jwt` in session", async () => {
     expect(sessionStorage.getItem("jwt")).toBe(null);
     expect(await isAuthenticated()).toBe(false);
     expect(axios.post).toHaveBeenCalledTimes(0);
   });
 
-  it("should return false when /rpc/login_check rejects", async () => {
-    const token = "aFakeToken";
-    sessionStorage.setItem("jwt", token);
+  it("should return false with no `me` in session", async () => {
+    sessionStorage.setItem("jwt", JWT);
 
-    expect(sessionStorage.getItem("jwt")).toBe(token);
+    expect(sessionStorage.getItem("jwt")).toBe(JWT);
+    expect(sessionStorage.getItem("me")).toBe(null);
+    expect(await isAuthenticated()).toBe(false);
+    expect(axios.post).toHaveBeenCalledTimes(0);
+  });
+
+  // eslint-disable-next-line max-len
+  it("should return false when `me` doesn't have agreements in its payload", async () => {
+    const oldMe = JSON.stringify({ payload: {} });
+    sessionStorage.setItem("me", oldMe);
+
+    expect(sessionStorage.getItem("me")).toBe(oldMe);
+    expect(await isAuthenticated()).toBe(false);
+    expect(axios.post).toHaveBeenCalledTimes(0);
+  });
+
+  it("should return false when /rpc/login_check rejects", async () => {
+    sessionStorage.setItem("me", ME);
+
+    expect(sessionStorage.getItem("me")).toBe(ME);
     expect(await isAuthenticated()).toBe(false);
     expect(axios.post).toHaveBeenCalledTimes(1);
   });
