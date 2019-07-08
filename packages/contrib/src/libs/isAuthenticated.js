d181bb711f0c9cd7e79f045cd7f8b3560ff60a0a
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-07-08 13:20:59 +0200

    contrib/libs/isAuthenticated: temporarely force JWT update (until August 8th)

--- packages/contrib/src/libs/isAuthenticated.js
+++ packages/contrib/src/libs/isAuthenticated.js
@@ -5,8 +5,15 @@ const API_URI = getConfig().publicRuntimeConfig.API_URI;
 
 export default async function isAuthenticated() {
   const token = sessionStorage.getItem("jwt");
+  const me = JSON.parse(sessionStorage.getItem("me"));
 
-  if (typeof token !== "string" || token.length === 0) return false;
+  if (
+    typeof token !== "string" ||
+    token.length === 0 ||
+    me.payload.agreements === undefined
+  ) {
+    return false;
+  }
 
   try {
     const { data } = await axios.post(`${API_URI}/rpc/login_check`, { token });
