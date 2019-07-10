ff61bd84c4bf20f30ddad84fcc923adeae378e79
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-07-10 18:38:40 +0200

    scripts/ci/simulate_e2e_tests: fix and improve workflow

--- scripts/ci/simulate_e2e_tests.sh
+++ scripts/ci/simulate_e2e_tests.sh
@@ -1,22 +1,40 @@
-# This script allow us to simulate a CI run locally.
+#!/bin/bash
 
-# Override the .env variable (because localhost is not available within the container):
+################################################################################
+# This script simulates a CI run locally (likely on a developer machine).
+################################################################################
+
+# Exit when any command fails:
+set -e
+
+ENV_FILE="$PWD/.env"
+GREEN="\033[0;32m"
+NC='\033[0m'
+
+# Override API_URI environment variable (declare in the .env file) because
+# localhost won't available within the container:
 # https://docs.docker.com/compose/networking/
 # https://docs.docker.com/compose/environment-variables/#the-env-file
 export API_URI=http://api:3000
+# This is useful to inform the master container entrypoint script that we need
+# to seed the database with some dummy data:
+export NODE_ENV=test
 
-# Build all the docker-compose services:
-docker-compose build --no-cache web test
+echo "${GREEN}> Removing db containers & its volume…${NC}"
+docker-compose rm -v db > /dev/nul
+echo "${GREEN}> Starting db…${NC}"
+docker-compose up -d db > /dev/nul
+echo "${GREEN}> Building master, test & web…${NC}"
+docker-compose build master test web > /dev/nul
+# Starting master will automatically migrate & seed the database:
+echo "${GREEN}> Migrating & seeding database…${NC}"
+docker-compose up -d master
 
-# Start, migrate and seed database:
-docker-compose up -d db
-yarn db:migrate
-yarn db:seed
-
-# Launch the e2e tests:
+echo "${GREEN}> Running e2e tests…${NC}"
 docker-compose up --abort-on-container-exit --exit-code-from test test
 
-# Because we don't want the CI simulation to impact our local tests:
+# Clean the environment:
 unset API_URI
-# https://docs.docker.com/compose/reference/rm/
-docker-compose rm -fs
+
+echo "${GREEN}> Stopping docker-dind, master, test & web…${NC}"
+docker-compose stop master test docker-dind web test > /dev/nul
