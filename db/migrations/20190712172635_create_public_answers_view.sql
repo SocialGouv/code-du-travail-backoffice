c229098ffb208e6992ced297c71f84c699e7bac0
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-07-15 15:23:35 +0200

    db/migrations/20190712172635_create_public_answers_view: create

---
+++ db/migrations/20190712172635_create_public_answers_view.sql
@@ -0,0 +1,21 @@
+-------------------------------------- UP --------------------------------------
+
+CREATE VIEW api.public_answers AS
+SELECT
+    id,
+    parent_id,
+    value,
+    generic_reference,
+    is_published,
+    created_at,
+    updated_at,
+    question_id,
+    agreement_id
+  FROM api.answers answers
+  WHERE answers.state = 'validated';
+
+GRANT SELECT ON api.public_answers TO anonymous;
+
+------------------------------------- DOWN -------------------------------------
+
+DROP VIEW api.public_answers;
