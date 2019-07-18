a3e4bd80aeb0156bbde48b0675418db7e38f6dd6
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-07-18 11:47:11 +0200

    scripts/data/extractLawRefs: remove duplicates

--- scripts/data/extractLawRefs.js
+++ scripts/data/extractLawRefs.js
@@ -39,7 +39,11 @@ const lawRefs = data.children
   .map(getArticleNum)
   .flat(Infinity)
   // Remove strange L1, L2, L3 & Annexes... results.
-  .filter(ref => ref.length > 2 && /^[DRL]/.test(ref))
+  .filter(ref => (ref.length > 2 && /^[DRL]/.test(ref)))
+  // Remove duplicates:
+  // https://stackoverflow.com/a/9229821/2736233
+  .sort()
+  .filter((item, pos, ary) => (!pos || item != ary[pos - 1]))
 
 fs.writeFileSync(
   `${__dirname}/../../packages/contrib/static/data/labor-law-references.json`,
