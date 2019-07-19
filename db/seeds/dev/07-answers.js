9e3c5bb73d3ddd3d6c7db4714be586cdd6681133
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-07-19 03:34:04 +0200

    db/seeds/dev/07-answers: update

--- db/seeds/dev/07-answers.js
+++ db/seeds/dev/07-answers.js
@@ -1,20 +1,55 @@
-exports.seed = async knex => {
-  global.spinner.start(`Generating answers...`)
+const PREVALUE =
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ` +
+  `tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ` +
+  `veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea ` +
+  `commodo consequat. Duis aute irure dolor in reprehenderit in voluptate ` +
+  `velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint ` +
+  `occaecat cupidatat non proident, sunt in culpa qui officia deserunt ` +
+  `mollit anim id est laborum.`;
 
-  const questions = await knex('api.questions')
-  const agreements = await knex('api.agreements')
+exports.seed = async knex => {
+  global.spinner.start(`Generating answers...`);
+
+  const questions = await knex("api.questions");
+  const agreements = await knex("api.agreements");
 
   for (let question of questions) {
-    global.spinner.start(`Generating answers: ${question.value}`)
+    global.spinner.start(`Generating answers: ${question.value}`);
 
-    const answers = agreements.map((agreement) => ({
-      value: '',
-      question_id: question.id,
-      agreement_id: agreement.id
-    }))
+    const answers = agreements.map((agreement) => {
+      if (Math.random() < 0.5) {
+        return {
+          state: "todo",
+          prevalue: "",
+          value: "",
+          question_id: question.id,
+          agreement_id: agreement.id
+        };
+      }
 
-    await knex('api.answers').insert(answers)
+      if (Math.random() < 0.5) {
+        return {
+          state: "draft",
+          prevalue: PREVALUE,
+          value: "",
+          question_id: question.id,
+          agreement_id: agreement.id,
+          user_id: '00000000-0000-4000-0000-000000000002'
+        };
+      }
+
+      return {
+        state: "pending_review",
+        prevalue: PREVALUE,
+        value: "",
+        question_id: question.id,
+        agreement_id: agreement.id,
+        user_id: '00000000-0000-4000-0000-000000000002'
+      };
+    });
+
+    await knex("api.answers").insert(answers);
   }
 
-  global.spinner.succeed(`Answers generated.`)
-}
+  global.spinner.succeed(`Answers generated.`);
+};
