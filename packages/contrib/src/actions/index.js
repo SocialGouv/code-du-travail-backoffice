c52787e332fb237f16c0468d6d6e74716f71503c
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-07-19 04:42:56 +0200

    contrib/actions/comments: create

--- packages/contrib/src/actions/index.js
+++ packages/contrib/src/actions/index.js
@@ -1,6 +1,7 @@
 import actionTypes from "./types";
 
 import * as answers from "./answers";
+import * as comments from "./comments";
 import * as modal from "./modal";
 
-export { actionTypes, answers, modal };
+export { actionTypes, answers, comments, modal };
