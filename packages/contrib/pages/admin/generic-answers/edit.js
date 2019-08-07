8f413b799b8e129a1e81a8a867ebddf858a30513
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-08-07 12:17:10 +0200

    contrib/pages/admin/generic-answers/edit: create

---
+++ packages/contrib/pages/admin/generic-answers/edit.js
@@ -0,0 +1,11 @@
+import { connect } from "react-redux";
+
+import { AdminAnwsersEditPage } from "../answers/edit";
+
+class AdminGenericAnwsersEditPage extends AdminAnwsersEditPage {}
+
+export default connect(({ answers, comments }) => ({
+  answers,
+  comments,
+  isGeneric: true
+}))(AdminGenericAnwsersEditPage);
