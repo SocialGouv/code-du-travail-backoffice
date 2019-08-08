a1a53c497d092d943aa078baaa735b14612a77ab
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-08-08 13:07:26 +0200

    db/seeds/dev/07-answers: complete states

--- db/seeds/dev/07-answers.js
+++ db/seeds/dev/07-answers.js
@@ -2,7 +2,10 @@ const PREVALUE =
   `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ` +
   `tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ` +
   `veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea ` +
-  `commodo consequat. Duis aute irure dolor in reprehenderit in voluptate ` +
+  `commodo consequat.`;
+
+const VALUE =
+  `Duis aute irure dolor in reprehenderit in voluptate ` +
   `velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint ` +
   `occaecat cupidatat non proident, sunt in culpa qui officia deserunt ` +
   `mollit anim id est laborum.`;
@@ -19,7 +22,7 @@ exports.seed = async knex => {
     const genericAnswer = {
       state: "pending_review",
       prevalue: "",
-      value: PREVALUE,
+      value: VALUE,
       question_id: question.id,
       agreement_id: null
     };
@@ -27,7 +30,7 @@ exports.seed = async knex => {
     await knex("api.answers").insert([genericAnswer]);
 
     const answers = agreements.map(agreement => {
-      if (question.index < 4) {
+      if (question.index < 7) {
         return {
           state: "todo",
           prevalue: "",
@@ -37,7 +40,7 @@ exports.seed = async knex => {
         };
       }
 
-      if (question.index < 5) {
+      if (question.index < 8) {
         return {
           state: "draft",
           prevalue: PREVALUE,
@@ -48,10 +51,32 @@ exports.seed = async knex => {
         };
       }
 
+      if (question.index < 9) {
+        return {
+          state: "pending_review",
+          prevalue: PREVALUE,
+          value: "",
+          question_id: question.id,
+          agreement_id: agreement.id,
+          user_id: "00000000-0000-4000-0000-000000000002"
+        };
+      }
+
+      if (question.index < 10) {
+        return {
+          state: "under_review",
+          prevalue: PREVALUE,
+          value: VALUE,
+          question_id: question.id,
+          agreement_id: agreement.id,
+          user_id: "00000000-0000-4000-0000-000000000002"
+        };
+      }
+
       return {
-        state: "pending_review",
+        state: "validated",
         prevalue: PREVALUE,
-        value: "",
+        value: VALUE,
         question_id: question.id,
         agreement_id: agreement.id,
         user_id: "00000000-0000-4000-0000-000000000002"
