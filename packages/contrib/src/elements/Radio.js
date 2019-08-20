435779686974c443a8e14288d25faf9ef2d034c4
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-08-20 12:22:08 +0200

    contrib/elements/Radio: create

---
+++ packages/contrib/src/elements/Radio.js
@@ -0,0 +1,31 @@
+/* eslint-disable max-len */
+
+import React from "react";
+import styled from "styled-components";
+
+import _Icon from "./Icon";
+
+const Icon = styled(_Icon)`
+  color: var(--color-text-blue);
+  cursor: ${({ selected }) => (!selected ? "pointer" : "auto")};
+  margin-right: 0.5rem;
+
+  :hover {
+    color: ${({ selected }) =>
+      !selected ? "var(--color-lapis-lazuli)" : "var(--color-text-blue)"};
+  }
+`;
+
+export default ({ onChange, options, ...props }) => {
+  return options.map(({ isSelected, label, value }, index) => (
+    <div key={index}>
+      <Icon
+        icon={`${isSelected ? "dot-" : ""}circle`}
+        onClick={() => onChange(value)}
+        selected={isSelected}
+        {...props}
+      />
+      {label}
+    </div>
+  ));
+};
