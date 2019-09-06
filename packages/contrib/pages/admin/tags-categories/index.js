d242f2d314d17edc81c9f37de2c71febb25d9f0d
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-06 08:45:08 +0200

    contrib/pages/admin/tags-categories/index: create

---
+++ packages/contrib/pages/admin/tags-categories/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import AdminIndex from "../../../src/components/AdminIndex";
+
+const COLUMNS = [
+  {
+    Header: "Intitulé",
+    accessor: "value"
+  }
+];
+
+export default () => (
+  <AdminIndex
+    apiPath="/tags_categories"
+    ariaLabels={{
+      cancelDeletionButton: `Bouton annulant la suppression de cette étiquette
+                            de la base de données`,
+      deleteButton: `Bouton confirmant la suppression de cette étiquette de la
+                    base de données`,
+      editButton: `Bouton redirigeant vers le formulaire d'édition des données
+                  de cette étiquette`,
+      newButton: `Bouton redirigeant vers le formulaire de création d'une
+                 nouvelle étiquette`,
+      removeButton: `Bouton supprimant cette étiquette de la base de données
+                    après confirmation`
+    }}
+    columns={COLUMNS}
+    slug="tag-category"
+    title="Étiquettes > Catégories"
+  />
+);
