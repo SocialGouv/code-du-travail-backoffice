f7f6484d015d8903e32774e3706e7cb404d81773
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-06 08:45:13 +0200

    contrib/pages/admin/tags-categories/new: create

---
+++ packages/contrib/pages/admin/tags-categories/new.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import AdminForm from "../../../src/components/AdminForm";
+
+const FIELDS = [
+  {
+    type: "input",
+    name: "value",
+    label: "Intitulé"
+  }
+];
+
+export default class AdminTagsCategoriesNewPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fields: FIELDS
+    };
+  }
+
+  render() {
+    return (
+      <AdminForm
+        apiPath="/tags_categories"
+        ariaLabels={{
+          cancelButton: `Bouton redirigeant vers la liste des catégories
+                        d'étiquette`,
+          createOrEditButton: `Bouton créant une nouvelle catégorie d'étiquette
+                              dans la base de données à partir des données du
+                              formulaire`
+        }}
+        fields={this.state.fields}
+        indexPath="/tags-categories"
+        title="Nouvelle catégorie d'étiquette"
+      />
+    );
+  }
+}
