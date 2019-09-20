3650ef85c28c03cebe8271304b9ee4a4604a6340
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-20 11:38:00 +0200

    contrib/server/routes: add /admin/answers/print route

--- packages/contrib/server/routes.js
+++ packages/contrib/server/routes.js
@@ -39,7 +39,6 @@ module.exports = function(nextApp) {
     await nextApp.render(ctx.req, ctx.res, "/admin/agreements/new", {});
     ctx.respond = false;
   });
-
   router.get("/admin/agreements/:id", async ctx => {
     await nextApp.render(ctx.req, ctx.res, "/admin/agreements/edit", {
       ...ctx.params
@@ -47,8 +46,8 @@ module.exports = function(nextApp) {
     ctx.respond = false;
   });
 
-  router.get("/admin/answers/new", async ctx => {
-    await nextApp.render(ctx.req, ctx.res, "/admin/answers/new", {});
+  router.get("/admin/answers/print", async ctx => {
+    await nextApp.render(ctx.req, ctx.res, "/admin/answers/print", {});
     ctx.respond = false;
   });
 
