6971d9774bcdb29711ae38eae96d4daafde5fe3e
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-20 17:05:52 +0200

    contrib/libs/numeral: create

---
+++ packages/contrib/src/libs/numeral.js
@@ -0,0 +1,24 @@
+import numeral from "numeral";
+
+numeral.register("locale", "fr", {
+  delimiters: {
+    thousands: " ",
+    decimal: ","
+  },
+  abbreviations: {
+    thousand: "k",
+    million: "m",
+    billion: "b",
+    trillion: "t"
+  },
+  ordinal: function(number) {
+    return number === 1 ? "er" : "ème";
+  },
+  currency: {
+    symbol: "€"
+  }
+});
+
+numeral.locale("fr");
+
+export default numeral;
