427642aa4c741a0df39c9fe471c7aa5b696b185d
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2019-09-20 15:53:22 +0200

    feat(admin): add filterable dashboard stats (#547)
    * contrib/components/Table: create
    
    * contrib/components/AdminIndex: add sortedBy prop
    
    * contrib/pages/admin/index: make stats sortable

---
+++ packages/contrib/src/components/Table.js
@@ -0,0 +1,58 @@
+import ReactTable from "react-table";
+import styled from "styled-components";
+
+export default styled(ReactTable)`
+  .rt-thead {
+    background-color: rgba(0, 0, 0, 0.02);
+    border-bottom: solid 1px rgba(0, 0, 0, 0.1);
+
+    &.-header {
+      box-shadow: none;
+    }
+  }
+
+  .rt-tr > .rt-th,
+  .rt-tr > .rt-td {
+    :first-child {
+      width: 40% !important;
+    }
+    :not(:first-child) {
+      width: 10% !important;
+    }
+
+    > div {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+  }
+
+  .rt-tr > .rt-th {
+    font-weight: 600;
+    text-align: left;
+    transition: none;
+
+    &.-sort-asc,
+    &.-sort-desc {
+      box-shadow: none;
+    }
+    &.-sort-asc {
+      border-bottom: solid 3px rgba(0, 0, 0, 0.6);
+      padding: 5px 5px 2px;
+    }
+    &.-sort-desc {
+      border-top: solid 3px rgba(0, 0, 0, 0.6);
+      padding: 2px 5px 5px;
+    }
+
+    &.-cursor-pointer:hover {
+      background-color: rgba(0, 0, 0, 0.05);
+    }
+  }
+
+  .rt-tr > .rt-td {
+    :not(:first-child) {
+      text-align: right;
+    }
+  }
+`;
