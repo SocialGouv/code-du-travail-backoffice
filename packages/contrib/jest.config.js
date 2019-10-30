76b6fe4abea2d23545b2601c50020f22d97acc61
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-10-30 16:09:14 +0100

    test: include all src paths

--- packages/contrib/jest.config.js
+++ packages/contrib/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectCoverage: true,
   // https://github.com/facebook/jest/issues/7331
-  // collectCoverageFrom: ["<rootDir>/src/**/*.js"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.js"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|svg|ttf|woff|woff2)$": "<rootDir>/tests/mappers/fileTransformer.js",
     "\\.css$": "identity-obj-proxy"
@@ -12,13 +12,7 @@ module.exports = {
     "<rootDir>/tests",
     // Page components should be tested in e2e:
     "<rootDir>/pages",
-    "<rootDir>/server",
-    "<rootDir>/src/actions",
-    "<rootDir>/src/reducers",
-    "<rootDir>/src/sagas",
-    "<rootDir>/src/constants.js",
-    "<rootDir>/src/store.js",
-    "<rootDir>/src/texts.js"
+    "<rootDir>/server"
   ],
   // https://github.com/facebook/create-react-app/issues/2007#issuecomment-296694661
   setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.js"],
