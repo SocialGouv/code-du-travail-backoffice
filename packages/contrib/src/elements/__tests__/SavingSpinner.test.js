164b3c1424e79ccd134f804de1972109e31be687
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-10-31 00:50:38 +0100

    test: complete SavingSpinner element coverage

--- packages/contrib/src/elements/__tests__/SavingSpinner.test.js
+++ packages/contrib/src/elements/__tests__/SavingSpinner.test.js
@@ -1,23 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+
 import SavingSpinner from "../SavingSpinner";
 
 describe("[Contrib] elements/<SavingSpinner />", () => {
-  it("should match snapshot", () => {
-    const { container } = render(<SavingSpinner />);
+  it(`should pass`, () => {
+    const $savingSpinner = testRender(<SavingSpinner />);
+    const $spinner = $savingSpinner.children[3];
 
-    expect(container).toMatchSnapshot();
+    expect($savingSpinner).toMatchSnapshot();
+    expect($savingSpinner).toHaveStyleRule("height", "64px");
+    expect($savingSpinner).toHaveStyleRule("width", "64px");
+    expect($spinner).toHaveStyleRule("border", "6px solid white");
+    expect($spinner).toHaveStyleRule("border-color", "white transparent transparent transparent");
+    expect($spinner).toHaveStyleRule("height", "51px");
+    expect($spinner).toHaveStyleRule("margin", "6px");
+    expect($spinner).toHaveStyleRule("width", "51px");
   });
 
-  it("should match snapshot with a custom `color` prop", () => {
-    const { container } = render(<SavingSpinner color="red" />);
+  it(`should pass with {color} = "red"`, () => {
+    const $savingSpinner = testRender(<SavingSpinner color="red" />);
+    const $spinner = $savingSpinner.children[3];
 
-    expect(container).toMatchSnapshot();
+    expect($spinner).toHaveStyleRule("border", "6px solid red");
+    expect($spinner).toHaveStyleRule("border-color", "red transparent transparent transparent");
   });
 
-  it("should match snapshot with a custom `size` prop", () => {
-    const { container } = render(<SavingSpinner size="32" />);
+  it(`should pass with {size} = "32"`, () => {
+    const $savingSpinner = testRender(<SavingSpinner size="32" />);
+    const $spinner = $savingSpinner.children[3];
 
-    expect(container).toMatchSnapshot();
+    expect($savingSpinner).toHaveStyleRule("height", "32px");
+    expect($savingSpinner).toHaveStyleRule("width", "32px");
+    expect($spinner).toHaveStyleRule("height", "26px");
+    expect($spinner).toHaveStyleRule("margin", "3px");
+    expect($spinner).toHaveStyleRule("width", "26px");
   });
 });
