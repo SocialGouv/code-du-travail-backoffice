35fa45cb6821693d56d5113e4544beed5754cf15
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2019-11-16 16:09:33 +0100

    test: improve helpers coverage (#752)

---
+++ packages/contrib/src/helpers/__tests__/isNode.test.js
@@ -0,0 +1,13 @@
+import isNode from "../isNode";
+
+describe("[Contrib] helpers/isNode()", () => {
+  it("should return FALSE when running on client side", () => {
+    expect(isNode()).toStrictEqual(false);
+  });
+
+  it("should return TRUE when running on server side", () => {
+    delete global.window;
+
+    expect(isNode()).toStrictEqual(true);
+  });
+});
