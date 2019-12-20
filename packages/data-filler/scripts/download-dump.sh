13c72b7b90655b1a0214b29c805878b3062315d7
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-12-20 06:59:19 +0100

    fix(datafiller): fetch dump script

--- packages/data-filler/scripts/download-dump.sh
+++ packages/data-filler/scripts/download-dump.sh
@@ -3,7 +3,7 @@
 # download master version of CDTN data
 
 #`curl https://api.github.com/repos/SocialGouv/code-du-travail-numerique/branches/master | jq -r ".commit.sha" -`
-SHA=bfe5ad25d7ec6694997bc90bae15243d74535713
+SHA=`curl https://api.github.com/repos/SocialGouv/code-du-travail-numerique/branches/master | jq -r ".commit.sha" -`
 
 CDTN_REGISTRY=registry.gitlab.factory.social.gouv.fr/socialgouv/code-du-travail-numerique
 
