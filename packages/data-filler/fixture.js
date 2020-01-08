7408edc716512172dd9eaa1118eb4ad49a9fbda2
Ivan Gabriele <ivan.gabriele@gmail.com>
2020-01-09 00:45:36 +0100

    fix(kinto): repair bucket & collections creation fixture

--- packages/data-filler/fixture.js
+++ packages/data-filler/fixture.js
@@ -10,27 +10,52 @@ const { KINTO_BUCKET, KINTO_URI } = process.env;
 
 const parseResponse = res => {
   if (res.status >= 400) {
-    throw new Error(`${res.url} [${res.status}] : ${res.statusText}`);
+    throw new Error(`${res.url}: [${res.status}] ${res.statusText}.`);
   }
 };
 
 const updateDatabase = async () => {
   try {
-    // create collections
+    let res;
+
+    // Create bucket:
+
+    res = await fetch(`${KINTO_URI}/buckets`, {
+      method: "POST",
+      body: JSON.stringify({
+        data: {
+          id: KINTO_BUCKET
+        }
+      }),
+      headers: {
+        // Authorization: `Basic ${Buffer.from("admin:password").toString("base64")}`,
+        "Content-Type": "application/json"
+      }
+    });
+
+    parseResponse(res);
+
+    // Create collections:
+
     const collections = ["ccns", "glossaire", "requetes", "themes"];
     for (const collection of collections) {
-      await fetch(`${KINTO_URI}/buckets/${KINTO_BUCKET}/collections/${collection}`, {
-        method: "PUT",
+      res = await fetch(`${KINTO_URI}/buckets/${KINTO_BUCKET}/collections`, {
+        method: "POST",
         body: JSON.stringify({
-          data: { id: collection }
+          data: {
+            id: collection
+          }
         }),
         headers: {
+          // Authorization: `Basic ${Buffer.from("admin:password").toString("base64")}`,
           "Content-Type": "application/json"
         }
-      }).then(parseResponse);
+      });
+
+      parseResponse(res);
     }
   } catch (err) {
-    console.error(`Error: ${err}`);
+    console.error(`Error: ${err.message}`);
   }
 };
 
