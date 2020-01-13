da6563ea2b1e6fe893582842c5101aa7a2d26e95
Ivan Gabriele <ivan.gabriele@gmail.com>
2020-01-13 18:02:10 +0100

    ci(travis): optimize build time

--- scripts/prod/start.sh
+++ scripts/prod/start.sh
@@ -14,8 +14,10 @@ export $(egrep -v '^#' .env | xargs)
 # the test container):
 export CONFIG_ONLY=false
 
-echo "⏳ Dumping current databases…"
-yarn db:backup
+if [ "$NODE_ENV" = "production" ]; then
+  echo "⏳ Dumping current databases…"
+  yarn db:backup
+fi
 
 echo "⏳ Stopping all existing DC containers…"
 docker-compose stop
@@ -26,8 +28,8 @@ yarn --frozen-lockfile --no-cache
 echo "⏳ Starting db container…"
 docker-compose up -d db
 
-echo "⏳ Starting elastic container…"
-docker-compose up -d elastic
+# echo "⏳ Starting elastic container…"
+# docker-compose up -d elastic
 
 # Buiding the web container before migrating is a strategy to let the db
 # container be up and ready before running the migrations.
@@ -39,23 +41,17 @@ else
   docker-compose build --no-cache web
 fi
 
-# Crate Kinto database for non-production environments:
+echo "⏳ Running database migrations…"
+yarn db:migrate
+
+# Seed databases for non-production environments:
 if [ "$NODE_ENV" != "production" ]; then
-  echo "⏳ Create Kinto database for non-production environment…"
-  yarn db:init
+  echo "⏳ Restoring databases snapshot for non-production environment…"
+  yarn db:snapshot:restore
 fi
 
 echo "⏳ Starting kinto container…"
 docker-compose up -d kinto
-
-echo "⏳ Running database migrations…"
-yarn db:migrate
-
-# Seed database for non-production environments:
-if [ "$NODE_ENV" != "production" ]; then
-  echo "⏳ Running database seeds for non-production environment…"
-  yarn db:seed
-fi
 
 echo "⏳ Starting web (and api) container…"
 docker-compose up -d web
