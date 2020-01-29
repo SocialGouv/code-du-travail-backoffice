5905fa3b655bf897418f200215ca0c27070b7241
Ivan Gabriele <ivan.gabriele@gmail.com>
2020-01-29 11:30:02 +0100

    fix(data-filler): handle non-existing kinto uri env var

--- packages/contrib/next.config.js
+++ packages/contrib/next.config.js
@@ -7,11 +7,15 @@ const {
   API_SCHEME,
   API_URI_DOCKER,
   DATA_FILLER_PATH,
-  KINTO_BUCKET,
-  KINTO_URI
+  DEV_KINTO_PORT,
+  KINTO_BUCKET
 } = process.env;
 
 const API_URI = `${API_SCHEME}://${API_DOMAIN}:${API_PORT_PUBLIC}`;
+const KINTO_URI =
+  process.env.KINTO_URI !== undefined
+    ? process.env.KINTO_URI
+    : `http://localhost:${DEV_KINTO_PORT}`;
 
 module.exports = withCss(
   // We use next-transpile-modules in order to transpile the data-filler package source so that it
--- packages/data-filler/src/kinto/client.js
+++ packages/data-filler/src/kinto/client.js
@@ -5,10 +5,8 @@ global.fetch = require("node-fetch");
 
 const KintoClient = require("kinto-http");
 
-const { KINTO_URI } = process.env;
-
 const getClient = () => {
-  const url = `${KINTO_URI}/v1`;
+  const url = `${process.env.KINTO_URI}/v1`;
   const kintoClient = new KintoClient(url, { headers: {} });
 
   return kintoClient;
