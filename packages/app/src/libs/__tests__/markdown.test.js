79fed1ea6548e5baa44a4566508a7951b49b06b5
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2020-03-27 04:17:52 +0100

    fix(app): include sub-nested bullets in printed answers (#981)

--- packages/app/src/libs/__tests__/markdown.test.js
+++ packages/app/src/libs/__tests__/markdown.test.js
@@ -1,33 +1,70 @@
+import prettier from "prettier";
+
 import markdown from "../markdown";
 
-// TODO Check sanitization.
 describe("lib/Markdown", () => {
   it("should return an instance of Markdown", () => {
     expect(markdown.constructor.name).toBe("Markdown");
   });
 
-  const htmlSource = `<h2>Ceci est un titre</h2>
+  const htmlSource =
+    `
+<h2>Ceci est un titre</h2>
 <h3>Ceci est un sous-titre</h3>
-<p>Ceci est un paragraphe<br />
-sur plusieurs<br />
-lignes</p>
+<p>
+  Ceci est un paragraphe<br />
+  sur plusieurs<br />
+  lignes
+</p>
+<hr />
 <blockquote>
   <p>Ceci est une citation.</p>
 </blockquote>
 <p>Ceci est <em>un texte en italique</em>.</p>
-<p>Ceci est <u>un texte souligné</u>.</p>
 <p>Ceci est <strong>un texte en gras</strong>.</p>
 <p>Ceci est <a href="https://www.example.com">un lien</a>.</p>
 <ul>
-<li>Ceci est une</li>
-<li>Liste ordonnée</li>
+  <li>
+    Ceci
+    <ul>
+      <li>est</li>
+    </ul>
+  </li>
+  <li>
+    une
+    <ul>
+      <li>
+        liste
+        <ul>
+          <li>ordonnée.</li>
+        </ul>
+      </li>
+    </ul>
+  </li>
 </ul>
 <ol>
-<li>Ceci est une</li>
-<li>Liste numérotée</li>
-</ol>`;
+  <li>
+    Ceci
+    <ol>
+      <li>est</li>
+    </ol>
+  </li>
+  <li>
+    une
+    <ol>
+      <li>
+        liste
+        <ol>
+          <li>numérotée.</li>
+        </ol>
+      </li>
+    </ol>
+  </li>
+</ol>
+  `.trim() + "\n";
 
-  const markdownSource = `## Ceci est un titre
+  const markdownSource = `
+## Ceci est un titre
 
 ### Ceci est un sous-titre
 
@@ -35,31 +72,43 @@ Ceci est un paragraphe<br>
 sur plusieurs<br>
 lignes
 
+---
+
 > Ceci est une citation.
 
-Ceci est *un texte en italique*.
-
-Ceci est <u>un texte souligné</u>.
+Ceci est _un texte en italique_.
 
 Ceci est **un texte en gras**.
 
-Ceci est [un lien](<https://www.example.com>).
+Ceci est [un lien](https://www.example.com).
 
-- Ceci est une
-- Liste ordonnée
+- Ceci
+  - est
+- une
+  - liste
+    - ordonnée.
 
-1. Ceci est une
-2. Liste numérotée`;
+1. Ceci
+   1. est
+2. une
+   1. liste
+      1. numérotée.
+    `.trim();
 
   describe("#fromHtml()", () => {
     it("should return the expected Markdown string", () => {
+      // console.debug(markdown.fromHtml(htmlSource));
+
       expect(markdown.fromHtml(htmlSource)).toBe(markdownSource);
     });
   });
 
   describe("#toHtml()", () => {
     it("should return the expected Markdown string", () => {
-      expect(markdown.toHtml(markdownSource)).toBe(htmlSource);
+      const result = markdown.toHtml(markdownSource);
+      const prettyResult = prettier.format(result, { parser: "html" });
+
+      expect(prettyResult).toBe(htmlSource);
     });
   });
 });
--- packages/app/src/libs/markdown.js
+++ packages/app/src/libs/markdown.js
@@ -1,70 +1,50 @@
-import sanitizeHtml from "sanitize-html";
-import showdown from "showdown";
-
-// https://github.com/showdownjs/showdown#valid-options
-const converter = new showdown.Converter({
-  noHeaderId: true,
-  simplifiedAutoLink: true,
-  underline: true,
-});
-
-// https://github.com/punkave/sanitize-html#node-recommended
-const ALLOWED_ATTRS = {
-  a: ["href"],
-};
-const ALLOWED_TAGS = [
-  "a",
-  "b",
-  "blockquote",
-  "br",
-  "em",
-  "h2",
-  "h3",
-  "i",
-  "li",
-  "ol",
-  "p",
-  "strong",
-  "u",
-  "ul",
-];
+import rehypeParse from "rehype-parse";
+import rehypeRemark from "rehype-remark";
+import rehypeSanitize from "rehype-sanitize";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import remarkStringify from "remark-stringify";
+import unified from "unified";
 
 /**
  * Convert HTML from/to Markdown and sanitize both input and output HTML.
+ *
+ * TODO Use "remark-lint" to lint Markdown instead of custom replacements.
+ * https://github.com/remarkjs/remark-lint
  */
 class Markdown {
   fromHtml(htmlSource) {
-    const cleanHtmlSource = this._cleanHtmlSource(htmlSource);
+    const markdownSource = unified()
+      .use(rehypeSanitize)
+      .use(rehypeParse)
+      .use(rehypeRemark)
+      .use(remarkStringify, {
+        gfm: true,
+        listItemIndent: "1",
+        rule: "-",
+        ruleSpaces: false,
+      })
+      .processSync(htmlSource)
+      .contents.trim()
+      .replace(/ {2}$/gm, "<br>")
+      .replace(/^( *-.*)\n{2,}( *-)/gm, "$1\n$2")
+      .replace(/^( *-.*)\n{2,}( *-)/gm, "$1\n$2")
+      .replace(/^( *\d+\..*)\n{2,}( *\d+\.)/gm, "$1\n$2")
+      .replace(/^( *\d+\..*)\n{2,}( *\d+\.)/gm, "$1\n$2");
 
-    return this._makeMarkdown(cleanHtmlSource);
+    return markdownSource;
   }
 
   toHtml(markdownSource) {
-    const htmlSource = converter.makeHtml(markdownSource);
+    const htmlSource = unified()
+      .use(remarkParse)
+      .use(remarkRehype)
+      .use(rehypeStringify)
+      .use(rehypeSanitize)
+      .processSync(markdownSource.replace(/<br>/g, " ".repeat(2))).contents;
 
-    return this._cleanHtmlSource(htmlSource);
-  }
-
-  _cleanHtmlSource(htmlSource) {
-    return sanitizeHtml(htmlSource, {
-      allowedAttributes: ALLOWED_ATTRS,
-      allowedTags: ALLOWED_TAGS,
-    });
-  }
-
-  _makeMarkdown(htmlSource) {
-    const markdownSource = converter.makeMarkdown(htmlSource);
-
-    // This fixes strange behaviors from showdown library.
-    // Handling underlines is still experimental:
-    // https://github.com/showdownjs/showdown#valid-options
-    return markdownSource
-      .replace(/<\/u>\n\n/g, "</u>")
-      .replace(/\n>\s*\n/g, "\n")
-      .replace(/^>\s+/gm, "> ")
-      .replace(/<!-- -->\n\n/g, "")
-      .replace(/<br>\n\n\s/g, "<br>\n")
-      .trim();
+    return htmlSource;
   }
 }
 
