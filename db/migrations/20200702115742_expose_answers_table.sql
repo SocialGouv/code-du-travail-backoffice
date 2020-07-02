e0a6e1618f0f067d7c56abde673fec6ae9d7a3b4
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2020-07-02 14:29:42 +0200

    feat(api): expose answers table (#1086)

---
+++ db/migrations/20200702115742_expose_answers_table.sql
@@ -0,0 +1,7 @@
+-------------------------------------- UP --------------------------------------
+
+GRANT SELECT ON api.answers TO anonymous;
+
+------------------------------------- DOWN -------------------------------------
+
+REVOKE SELECT ON api.answers FROM anonymous;
