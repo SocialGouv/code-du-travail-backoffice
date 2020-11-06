1b8d929116f851e24c7b26244087f8bb57e15380
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-11-06 01:54:44 +0100

    fix: use build variables in gitlab-ci

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -2,7 +2,7 @@ const withCss = require("@zeit/next-css");
 
 const { API_DOMAIN, API_PORT_PUBLIC, API_SCHEME, API_URI_DOCKER, CDTN_API_URL } = process.env;
 
-const API_URI = `${API_SCHEME}://${API_DOMAIN}:${API_PORT_PUBLIC}`;
+const API_URI = `${API_SCHEME}://${API_DOMAIN}${API_PORT_PUBLIC ? `:${API_PORT_PUBLIC}` : ``}`;
 
 module.exports = withCss({
   // https://nextjs.org/docs#build-time-configuration
