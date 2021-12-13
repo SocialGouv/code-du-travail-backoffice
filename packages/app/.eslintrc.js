9bef17c7cee057f223dd52ef82a84a82e72f3c14
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-12-13 10:02:58 +0100

    fix: linter

--- packages/app/.eslintrc.js
+++ packages/app/.eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "jest", "only-warn"],
   root: true,
+  env: {
+    "jest/globals": true,
+    browser: true,
+    node: true,
+  },
 };
