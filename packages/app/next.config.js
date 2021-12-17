9c9c1d57938b8ec4846616f03efaa3bfb91a2d50
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-12-17 10:22:15 +0100

    fix(app): build is now working

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,14 +1,12 @@
-const withCss = require("@zeit/next-css");
-
 const { API_DOMAIN, API_PORT_PUBLIC, API_SCHEME, API_URI_DOCKER, CDTN_API_URL } = process.env;
 
 const API_URI = `${API_SCHEME}://${API_DOMAIN}:${API_PORT_PUBLIC}`;
 
-module.exports = withCss({
+module.exports = {
   // https://nextjs.org/docs#build-time-configuration
   env: {
     API_URI,
     API_URI_DOCKER,
     CDTN_API_URL,
   },
-});
+};
