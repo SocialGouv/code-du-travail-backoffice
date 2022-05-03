512ef3af151aaf3f512f32cadca542df12b90220
Martial Maillot <martial.maillot@gmail.com>
2022-05-03 17:53:52 +0200

    fix: create a new question

---
+++ packages/app/pages/admin/questions/new.jsx
@@ -0,0 +1,61 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+import { addQuestion } from "../../../src/api";
+import AdminForm from "../../../src/components/AdminForm";
+
+export default function AdminNew() {
+  const { back } = useRouter();
+  const [isSubmitting, submitting] = React.useState(false);
+  const [error, setError] = React.useState();
+
+  const FIELDS = [
+    {
+      inputType: "number",
+      label: "Index",
+      name: "index",
+      type: "input",
+    },
+    {
+      label: "Intitulé",
+      name: "value",
+      type: "text",
+    },
+  ];
+
+  const onSubmit = async data => {
+    try {
+      submitting(true);
+      setError(null);
+      await addQuestion(data.index, data.value);
+      back();
+    } catch (err) {
+      if (
+        err !== undefined &&
+        err.response !== undefined &&
+        err.response.data !== undefined &&
+        typeof err.response.data.message === "string"
+      ) {
+        setError(`Erreur: ${err.response.data.message}.`);
+      } else {
+        setError(`Erreur: ${err}`);
+        if (err !== undefined) console.warn(err);
+      }
+    } finally {
+      submitting(false);
+    }
+  };
+
+  return (
+    <AdminForm
+      isSubmitting={isSubmitting}
+      apiPath="/questions"
+      fields={FIELDS}
+      i18nIsFeminine={true}
+      i18nSubject="question"
+      indexPath="/questions"
+      onSubmit={onSubmit}
+      error={error}
+    />
+  );
+}
--- packages/app/src/api/index.js
+++ packages/app/src/api/index.js
@@ -1,1 +1,2 @@
 export * from "./agreements";
+export * from "./questions";
